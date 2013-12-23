0e50e80110731910706c5f18c57d419430efdee5
Your Name Here <adam@ehealthafrica.org>
2013-12-23 16:41:10 +0100

    initial commit with chromeapp and angular dir structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lmisChromeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
