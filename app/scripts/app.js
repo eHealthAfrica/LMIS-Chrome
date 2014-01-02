8496746d4cbe39c79a47ab6b9ac4a2a889a8ecc2
Your Name Here <adam@ehealthafrica.org>
2014-01-02 14:21:23 +0100

    tested online offline events and added to app scope

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 angular.module('lmisChromeApp', [
   'ngCookies',
   'ngResource',
@@ -16,3 +17,20 @@ angular.module('lmisChromeApp', [
         redirectTo: '/'
       });
   });
+
+/* Central Variable for Watching Online/Offline Events */
+angular.module('lmisChromeApp')
+    .run(function($window, $rootScope) {
+        $rootScope.online = navigator.onLine;
+        console.log("online" + $rootScope.online);
+        $window.addEventListener("offline", function () {
+            $rootScope.$apply(function() {
+                $rootScope.online = false;
+            });
+        }, false);
+        $window.addEventListener("online", function () {
+            $rootScope.$apply(function() {
+                $rootScope.online = true;
+            });
+        }, false);
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('lmisChromeApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    /*$scope.$watch('online', function(newStatus) {})*/
   });
