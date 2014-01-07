692175dbe67a920fd20e865ad99fe3c1b13e5a05
Your Name Here <adam@ehealthafrica.org>
2014-01-07 10:02:09 +0100

    using chrome storage in settings to persist data, will not work outside of chrome app now.

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -3,4 +3,41 @@
 angular.module('lmisChromeApp')
     .controller('SettingsCtrl', function ($scope) {
 
+        // var facility = $scope.facility = {};
+
+        /* TODO: come up with chrome.storage.local provider ie: http://gregpike.net/demos/angular-local-storage/demo/demo.html */
+        chrome.storage.local.get('facility', function(value){
+            $scope.$apply(function() {
+                $scope.load(value);
+            });
+        });
+
+        $scope.load = function(value) {
+            if (value && value.facility) {
+                $scope.facility = value.facility;
+            } else {
+                $scope.facility = {};
+                /* TODO: get defaults from Facility Factory */
+            }
+        };
+
+        $scope.save = function() {
+             chrome.storage.local.set({'facility': $scope.facility});
+        };
+
+        /* LMIS App Settings
+        * Facility Name: [can be a drop down generated from a json file in filesystemstorage or retrieved from web service
+        *                 based on site UUID and SecretKey]
+        * Facility Type: [drop down or web service]
+        * GlobalID: [uuid for chrome app installation, generated 1st run only. used to retreive data from server]
+        * SecretKey: [random generated key for use in matching GlobalID, generated at server or app?]
+        * Facility Users/Staff: [name, position/title, contact details for facility staff]
+        *
+        *
+        *
+        *
+        * Inspect Chrome Storage:
+        *                   chrome.storage.local.get(null, console.log.bind(console));  */
+
     });
+
