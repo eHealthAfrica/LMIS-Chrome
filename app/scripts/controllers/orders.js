ed076d09c62ab4e81893e76f4ea071eba7b48fbf
Musa Musa <musakunte@gmail.com>
2014-01-08 21:21:23 +0100

    added script to save and retrieve data from chrome local storage

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -2,9 +2,41 @@
 
 angular.module('lmisChromeApp')
   .controller('OrdersctrlCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+         //chrome.storage.local.set({'order_list':'' });
+        //populate order object with data if available in storage
+        chrome.storage.local.get('order_list', function(value){
+            $scope.$apply(function(){
+                $scope.load(value);
+            });
+        });
+
+        $scope.load = function(value){
+            if(value && value.order_list){
+                $scope.orders = value.order_list;
+            }
+            else{
+                $scope.orders = {};
+            }
+
+        }
+        //save data when save button is clicked
+        $scope.save = function() {
+
+          chrome.storage.local.set({'order_list':$scope.orders });
+        };
+
+        $scope.$watch('stored_data', function(){
+            chrome.storage.local.get(null, function(storage) {
+                $scope.$apply(function() {
+                    if (storage) {
+                        $scope.stored_data = storage.order_list;
+                    } else {
+                        $scope.stored_data = {};
+                    }
+                });
+            });
+
+        });
+
+
   });
