6d71b03e6978d2fa28ef3033c37dd1f339fbb458
Musa Musa <musakunte@gmail.com>
2014-01-09 16:32:10 +0100

    store data in array list

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -2,7 +2,7 @@
 
 angular.module('lmisChromeApp')
   .controller('OrdersctrlCtrl', function ($scope) {
-         //chrome.storage.local.set({'order_list':'' });
+        //chrome.storage.local.set({'order_list':[] });
         //populate order object with data if available in storage
         chrome.storage.local.get('order_list', function(value){
             $scope.$apply(function(){
@@ -12,18 +12,16 @@ angular.module('lmisChromeApp')
 
         $scope.load = function(value){
             if(value && value.order_list){
-                $scope.orders = value.order_list;
+                $scope.orders = value.order_list.reverse()[0];
             }
             else{
                 $scope.orders = {};
             }
 
         }
-        //save data when save button is clicked
-        $scope.save = function() {
 
-          chrome.storage.local.set({'order_list':$scope.orders });
-        };
+
+
 
         $scope.$watch('stored_data', function(){
             chrome.storage.local.get(null, function(storage) {
@@ -37,6 +35,15 @@ angular.module('lmisChromeApp')
             });
 
         });
-
+        $scope.data_storage = [];
+        //save data when save button is clicked
+        $scope.save = function() {
+          //check for if we have data stored in local storage
+          if($scope.stored_data.length>0){
+              $scope.data_storage = $scope.stored_data;
+          }
+          $scope.data_storage.push($scope.orders);
+          chrome.storage.local.set({'order_list': $scope.data_storage});
+        };
 
   });
