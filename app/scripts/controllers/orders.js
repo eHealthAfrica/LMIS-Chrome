0702dcf17129f28502709683d2b83424e2f4149f
Musa Musa <musakunte@gmail.com>
2014-01-10 16:53:11 +0100

    implemented uuid

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .controller('OrdersctrlCtrl', function ($scope) {
-        //chrome.storage.local.set({'order_list':[] });
+  .controller('OrdersctrlCtrl', function ($scope, utility) {
+        chrome.storage.local.set({'order_list':[], 'orders_list':[], 'facility':[] });
         //populate order object with data if available in storage
         chrome.storage.local.get('order_list', function(value){
             $scope.$apply(function(){
@@ -11,17 +11,17 @@ angular.module('lmisChromeApp')
         });
 
         $scope.load = function(value){
-            if(value && value.order_list){
-                $scope.orders = value.order_list.reverse()[0];
+            if(value && value.order_list.length > 0){
+
+                var index = value.order_list.length - 1;
+                $scope.orders = value.order_list[index];
+                $scope.orders_index = index;
             }
             else{
                 $scope.orders = {};
             }
 
         }
-
-
-
 
         $scope.$watch('stored_data', function(){
             chrome.storage.local.get(null, function(storage) {
@@ -42,6 +42,10 @@ angular.module('lmisChromeApp')
           if($scope.stored_data.length>0){
               $scope.data_storage = $scope.stored_data;
           }
+
+          $scope.orders.synced=0;
+          $scope.orders.order_status=0;
+          $scope.orders.uuid = utility.getUUID();
           $scope.data_storage.push($scope.orders);
           chrome.storage.local.set({'order_list': $scope.data_storage});
         };
