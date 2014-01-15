41e4b21c76370a71ca862e7d6e9d400acba33345
Musa Musa <musakunte@gmail.com>
2014-01-15 05:45:56 +0100

    implemented storageService for order

---
+++ app/scripts/services/settingsfactory.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('lmisChromeApp')
+  .factory('settingsService', function ($q, storageService) {
+
+    function getSettingsFromStorage() {
+      var defered = $q.defer();
+      storageService.get('settings').then(function(settings) {
+        defered.resolve(settings);
+      });
+      return defered.promise;
+    }
+
+    function saveSettings(settings) {
+      storageService.add('settings', settings).then();
+      // TODO: convert to promise and send message on successful save, probably make generic in storageprovider
+    }
+
+    return {
+      load: getSettingsFromStorage,
+      save: saveSettings
+    };
+
+  });
