26b6856b9f5b2a56864901f7e53161355743f2dd
Musa Musa <musakunte@gmail.com>
2014-01-19 23:10:45 +0100

    added functions for date picker Refs #76

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -1,12 +1,51 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .controller('OrdersctrlCtrl', function ($scope, utility, ChromeStorageService) {
-        $scope.codec = "linka";
+  .controller('OrdersctrlCtrl', function ($scope, utility, storageService) {
+        $scope.today = function() {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.showWeeks = true;
+        $scope.toggleWeeks = function () {
+            $scope.showWeeks = ! $scope.showWeeks;
+        };
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+            return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+            $scope.minDate = ( $scope.minDate ) ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+
+            $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+            'year-format': "'yy'",
+            'starting-day': 1
+        };
+
+        $scope.format = 'yyyy-MM-dd';
+
+
+
         //chrome.storage.local.clear();
         //populate order object with data if available in storage
 
-        ChromeStorageService.get('order_list').then(function(value){
+        storageService.get('order_list').then(function(value){
            $scope.load(value);
         });
 
