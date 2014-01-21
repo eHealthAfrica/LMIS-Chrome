07ec7526f768a30f6f40c07680db028bb15e3af2
Musa Musa <musakunte@gmail.com>
2014-01-21 21:09:11 +0100

    added extra checks for active menu selection  Refs #92

--- app/scripts/directives/nav.js
+++ app/scripts/directives/nav.js
@@ -9,16 +9,20 @@ angular.module('lmisChromeApp')
                 scope.$watch(function() {
                     return $location.path();
                 }, function(newValue, oldValue) {
-                    console.log(newValue);
                     $('li[data-match-route]', element).each(function(k, li) {
                         var $li = angular.element(li),
                             pattern = $li.attr('data-match-route'),
                             regexp = new RegExp('^' + pattern + '$', ['i']);
-
-                        if(regexp.test(newValue)) {
+                        //console.log("p = "+pattern);
+                        var link_arr = $location.path().replace(/^\//,'').split('/');
+                        if(link_arr[0] == pattern.replace(/^\//,'')){
                             $li.addClass('active');
-                        } else {
-                            $li.removeClass('active');
+                        }else{
+                            if(regexp.test(newValue)) {
+                                $li.addClass('active');
+                            } else {
+                                $li.removeClass('active');
+                            }
                         }
                     });
                 });
