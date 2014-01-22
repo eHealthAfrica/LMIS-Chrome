0bcea9baf7346f2c0d9cf4832441ff20f4eaa334
Musa Musa <musakunte@gmail.com>
2014-01-22 21:19:05 +0100

    code to load products from local storage Refs #92

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-    .controller('MainCtrl', function ($scope) {
-        //$scope.addbreadcrumbs([]);
+    .controller('MainCtrl', function ($scope, storageService, $location ) {
+        var url_arr = $location.path().replace(/\/$/,'').replace(/^\//,'').split('/');
+        var bc = [];
+        if(url_arr.indexOf('products') != -1){
+            bc = [{name:"Products", "link":''}];
+        }
+        else if(url_arr.indexOf('product_form') != -1){
+            bc =
+            [
+                {name:"Products", "link":'#/main/products'},
+                {name:"Form", "link":''}
+            ];
+        }
+        else{
+            bc = [];
+        }
+        $scope.addbreadcrumbs(bc);
+
+        storageService.get('products').then(function(d){
+           $scope.products = d;
+        });
+        //console.log($scope.products);
         /*$scope.$watch('online', function(newStatus) {})*/
     });
