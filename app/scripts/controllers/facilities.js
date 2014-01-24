3b467fea7d457c9b4dbbf9fe87eaa9c0420a25e3
Musa Musa <musakunte@gmail.com>
2014-01-24 01:52:32 +0100

    first draft of facility list view  Refs #99

---
+++ app/scripts/controllers/facilities.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('lmisChromeApp')
+  .controller('FacilitiesCtrl', function ($scope, storageService, utility) {
+    storageService.get('facility').then(function(data){
+         $scope.facilities = data;
+    });
+
+   $scope.getUser = function(created_by){
+       return created_by.username;
+   }
+  utility.loadTableObject('facility_type');
+  storageService.get('re_facility_type').then(function(data){
+             $scope.facility_types = data;
+   });
+
+  });
