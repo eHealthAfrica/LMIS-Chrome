eb11528e62e3749a0caeb33887a699aac96dff3e
= <jideobi.ofomah@ehealthnigeria.org>
2014-01-26 10:12:27 +0100

    added breadcrumb navigation to product item Refs #105

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,11 +13,18 @@ chromeApp.controller('MainCtrl', function ($scope, storageService, $location ) {
                     {name:"Product", "link":'#/main/products'},
                     {name:"Product Item", "link":''}
                  ];
+        }else if(url_arr.indexOf('product_item_form') != -1){
+            bc =
+            [
+                {name:"Product", "link":'#/main/products'},
+                {name:"Product Item", "link":'#main/product_items'},
+                {name:"Add Product Item", "link":''}
+            ];
         }
         else if(url_arr.indexOf('product_form') != -1){
             bc =
             [
-                {name:"Products", "link":'#/main/products'},
+                {name:"Product", "link":'#/main/products'},
                 {name:"Add Product", "link":''}
             ];
         }
@@ -117,6 +124,70 @@ chromeApp.controller('ProductItemListCtrl', function($scope, storageService){
     storageService.get(storageService.PRODUCT_PRESENTATION).then(function(presentationList){
            $scope.presentations = presentationList;
     });
+
+    storageService.get(storageService.COMPANY).then(function(companyList){
+           $scope.companies = companyList;
+    });
+
+    storageService.get(storageService.CURRENCY).then(function(currencyList){
+           $scope.currencies = currencyList;
+    });
+
+    storageService.get(storageService.CURRENCY).then(function(currencyList){
+           $scope.currencies = currencyList;
+    });
+
+    storageService.get(storageService.MODE_OF_ADMINISTRATION).then(function(modeOfAdministrationList){
+           $scope.modes = modeOfAdministrationList;
+    });
+
+
+
+
+    //expiration date picker UI parameters
+    $scope.maxDate = "'2050-12-31'";
+
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
+
+    $scope.showWeeks = true;
+
+    $scope.toggleWeeks = function () {
+        $scope.showWeeks = ! $scope.showWeeks;
+    };
+
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
+
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
+
+    $scope.toggleMin = function() {
+        $scope.minDate = ( $scope.minDate ) ? null : new Date();
+    };
+    $scope.toggleMin();
+
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+    $scope.dateOptions = {
+        'year-format': "'yyyy'",
+        'starting-day': 1
+    };
+
+    $scope.format = 'yyyy-MM-dd';
+
+
+
 
  });
 
