68a196f688c46e35469b891fd5e9774b792d1fe6
Tom Vincent <git@tlvince.com>
2014-01-27 20:58:57 +0000

    JSHint facilities controller

--- app/scripts/controllers/facilities.js
+++ app/scripts/controllers/facilities.js
@@ -1,20 +1,19 @@
 'use strict';
+// jshint camelcase: false
 
 angular.module('lmisChromeApp')
-  .controller('FacilitiesCtrl', function ($scope, storageService, utility) {
+  .controller('FacilitiesCtrl', function($scope, storageService, utility) {
 
-    storageService.get(storageService.FACILITY).then(function(data){
-
-         $scope.facilities = data;
+    storageService.get(storageService.FACILITY).then(function(data) {
+      $scope.facilities = data;
     });
 
-   $scope.getUser = function(created_by){
-       return created_by.username;
-   }
+    $scope.getUser = function(created_by) {
+      return created_by.username;
+    };
 
-  utility.loadTableObject(storageService.FACILITY_TYPE).then(function(data){
+    utility.loadTableObject(storageService.FACILITY_TYPE).then(function(data) {
       $scope.facility_types = data;
-  });
-
+    });
 
   });
