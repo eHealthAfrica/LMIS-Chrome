ac2ec418f64a399ff8bb7fab197d4cba4b41260b
= <jideobi.ofomah@ehealthnigeria.org>
2014-01-27 22:51:02 +0100

    added CCE form, started formatting CCE list view to display meaningful data Refs #109

--- app/scripts/controllers/cce.js
+++ app/scripts/controllers/cce.js
@@ -2,8 +2,18 @@
 
 var chromeApp = angular.module('lmisChromeApp');
 
-chromeApp.controller('cceCtrl', function ($scope, $location) {
+chromeApp.controller('cceCtrl', function ($scope, storageService, utility) {
+    storageService.get(storageService.STORAGE_LOCATION).then(function(cceList) {
+      $scope.cceList = cceList;
+    });
 
+    utility.loadTableObject(storageService.FACILITY).then(function(facilities) {
+      $scope.facilities = facilities;
+    });
+
+    utility.loadTableObject(storageService.UOM).then(function(uomList) {
+      $scope.uomList = uomList;
+    });
 });
 
 
@@ -11,8 +21,21 @@ chromeApp.controller('cceCtrl', function ($scope, $location) {
  *  This controller is used to save CCE record to local storage or remote DB via REST API.
  *
  */
-chromeApp.controller('addCCECtrl', function($scope){
+chromeApp.controller('addCCECtrl', function($scope, storageService){
     //default cce to hold form data
     $scope.cce = {}
+
+    storageService.get(storageService.UOM).then(function(uomList) {
+      $scope.uomList = uomList;
+    });
+
+    storageService.get(storageService.FACILITY).then(function(facilities) {
+      $scope.facilities = facilities;
+    });
+
+    storageService.get(storageService.STORAGE_LOCATION).then(function(cceList) {
+      $scope.cceList = cceList;
+    });
+
 
 });
