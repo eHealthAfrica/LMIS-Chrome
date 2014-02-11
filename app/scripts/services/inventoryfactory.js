9de7909f41e31fcdff3d996afef80b62cd03d90e
Musa Musa <musakunte@gmail.com>
2014-02-11 22:43:22 +0100

    moved core logic from inventory controller to inventory factory   Refs #76

--- app/scripts/services/inventoryfactory.js
+++ app/scripts/services/inventoryfactory.js
@@ -1,16 +1,36 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .factory('inventoryFactory', function () {
-    // Service logic
-    // ...
+  .factory('inventoryFactory', function ($q, storageService, $location, $route) {
 
-    var meaningOfLife = 42;
+    var stock_records={
+        save_record_profile:function (record_profile_object) {
+            storageService.insert('monthly_stock_record', record_profile_object).then(function (bool) {
+                  $route.reload();
+            });
+          },
+        status_column:function(stock_products){
+            var table_html = '<td></td>';
+            for(var i=0; i<stock_products.length; i++){
+                table_html += "<td>Received</td><td>Used</td><td>Balance</td>";
+            }
+            return table_html;
+        },
+        //stock brought forward from prvious month
+        brought_forward_columns:function (monthly_stock_record_object, stock_products){
+        var table_html = '<td>BBF</td>';
+        for(var i=0; i<stock_products.length; i++){
+            table_html += '<td>'+ monthly_stock_record_object.balance_brought_forward[i]+'</td>'+
+                          '<td></td>'+
+                          '<td></td>';
+        }
+        return table_html;
+    }
+    }
 
-    // Public API here
+
+
     return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
+      stock_records: stock_records
     };
   });
