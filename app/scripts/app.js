bea150076d14e08bda3e87c56cba914e56184a44
Tom Vincent <git@tlvince.com>
2014-02-13 15:31:02 +0000

    Lint main app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,76 +1,76 @@
 'use strict';
 
 angular.module('lmisChromeApp', [
-      'ngCookies',
-      'ngResource',
-      'ngSanitize',
-      'ngRoute',
-      'restangular',
-      'ui.bootstrap',
-      'ngTable'
-    ])
-    .config(function ($routeProvider) {
-      // TODO: change the router to ui-router:
-      // https://github.com/angular-ui/ui-router/wiki/Quick-Reference
-      $routeProvider
-          .when('/', {
-            templateUrl: 'views/main/dashboard.html',
-            controller: 'MainCtrl'
-          })
-          .when('/home', {
-            templateUrl: 'views/main/dashboard.html',
-            controller: 'MainCtrl'
-          })
-          .when('/main/:template', {
-            templateUrl: function (elem) {
-              return 'views/main/' + elem.template + '.html';
-            },
-            controller: 'MainCtrl'
-          })
-          .when('/settings', {
-            templateUrl: 'views/settings.html',
-            controller: 'SettingsCtrl'
-          })
-          .when('/orders/:template', {
-            templateUrl: function (elem) {
-              return 'views/orders/' + elem.template + '.html';
-            },
-            controller: 'OrdersctrlCtrl'
-          })
-          .when('/facilities/:template', {
-            templateUrl: function (elem) {
-              return 'views/facilities/' + elem.template + '.html';
-            },
-            controller: 'FacilitiesCtrl'
-          })
-          .when('/inventory/:template', {
-            templateUrl: function (elem) {
-              return 'views/inventory/' + elem.template + '.html';
-            },
-            controller: 'InventoryCtrl'
-          })
-          .when('/cce/:template', {
-            templateUrl: function (elem) {
-              return 'views/cce/' + elem.template + '.html';
-            },
-            controller: 'cceCtrl'
-          })
-          .when('/products/:template', {
-            templateUrl: function (elem) {
-              return 'views/products/' + elem.template + '.html';
-            },
-            controller: 'ProductListCtrl'
-          })
-          .when('/batches/', {
-            templateUrl: function (elem) {
-              return 'views/batches/index.html';
-            },
-            controller: 'BatchListCtrl'
-          })
-          .otherwise({
-            redirectTo: '/'
-          });
-    })
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'restangular',
+  'ui.bootstrap',
+  'ngTable'
+])
+  .config(function($routeProvider) {
+    // TODO: change the router to ui-router:
+    // https://github.com/angular-ui/ui-router/wiki/Quick-Reference
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main/dashboard.html',
+        controller: 'MainCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'views/main/dashboard.html',
+        controller: 'MainCtrl'
+      })
+      .when('/main/:template', {
+        templateUrl: function(elem) {
+          return 'views/main/' + elem.template + '.html';
+        },
+        controller: 'MainCtrl'
+      })
+      .when('/settings', {
+        templateUrl: 'views/settings.html',
+        controller: 'SettingsCtrl'
+      })
+      .when('/orders/:template', {
+        templateUrl: function(elem) {
+          return 'views/orders/' + elem.template + '.html';
+        },
+        controller: 'OrdersctrlCtrl'
+      })
+      .when('/facilities/:template', {
+        templateUrl: function(elem) {
+          return 'views/facilities/' + elem.template + '.html';
+        },
+        controller: 'FacilitiesCtrl'
+      })
+      .when('/inventory/:template', {
+        templateUrl: function(elem) {
+          return 'views/inventory/' + elem.template + '.html';
+        },
+        controller: 'InventoryCtrl'
+      })
+      .when('/cce/:template', {
+        templateUrl: function(elem) {
+          return 'views/cce/' + elem.template + '.html';
+        },
+        controller: 'cceCtrl'
+      })
+      .when('/products/:template', {
+        templateUrl: function(elem) {
+          return 'views/products/' + elem.template + '.html';
+        },
+        controller: 'ProductListCtrl'
+      })
+      .when('/batches/', {
+        templateUrl: function() {
+          return 'views/batches/index.html';
+        },
+        controller: 'BatchListCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
 
   // Config Block for Restangular Provider
   .config(function(RestangularProvider, $compileProvider) {
@@ -78,10 +78,9 @@ angular.module('lmisChromeApp', [
     $compileProvider.aHrefSanitizationWhitelist(
       /^\s*(https?|ftp|mailto|chrome-extension):/
     );
-  });
+  })
 
-// Central Variable for Watching Online/Offline Events
-angular.module('lmisChromeApp')
+  // Central Variable for Watching Online/Offline Events
   .run(function($window, $rootScope, storageService) {
     //global message placeholder
     $rootScope.messages = [];
