3f1b478a9601f4fbe04e11cc8f3bca4996707ff5
Tom Vincent <git@tlvince.com>
2014-02-21 23:51:50 +0000

    Remove unused angular-route from karma

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,6 @@ module.exports = function(config) {
       'app/bower_components/angular-resource/angular-resource.js',
       'app/bower_components/angular-cookies/angular-cookies.js',
       'app/bower_components/angular-sanitize/angular-sanitize.js',
-      'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/lodash/dist/lodash.min.js',
       'app/bower_components/restangular/dist/restangular.min.js',
       'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
