7fd94ec76316bf219f84aaa34dc2282004a930b9
Tom Vincent <git@tlvince.com>
2014-03-05 16:40:09 +0000

    Default Karama to single run mode

--- Gruntfile.js
+++ Gruntfile.js
@@ -284,8 +284,7 @@ module.exports = function(grunt) {
     // Test settings
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
-        singleRun: true
+        configFile: 'karma.conf.js'
       }
     },
 
