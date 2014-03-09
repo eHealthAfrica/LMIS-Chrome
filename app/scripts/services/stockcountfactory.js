4dd8edf510032f73b62b3f92aa1b122d4151e485
Musa Musa <musakunte@gmail.com>
2014-03-09 15:02:56 +0100

    added an object of months

--- app/scripts/services/stockcountfactory.js
+++ app/scripts/services/stockcountfactory.js
@@ -3,7 +3,7 @@
 angular.module('lmisChromeApp')
     .factory('stockCountFactory', function ($q, storageService, $http) {
 
-    var discarded_reasons = {
+      var discarded_reasons = {
         "0": "VVM Stage 3",
         "1": "Broken Vial",
         "2": "Label Missing",
@@ -11,8 +11,22 @@ angular.module('lmisChromeApp')
         "4": "Opened Expiry",
         "5": "Suspected Freezing",
         "6": "Other"
-    }
-    var program_products= [
+      }
+      var months = {
+        '01': 'January',
+        '02': 'February',
+        '03': 'March',
+        '04': 'April',
+        '05': 'May',
+        '06': 'June',
+        '07': 'July',
+        '08': 'August',
+        '09': 'September',
+        '10': 'October',
+        '11': 'November',
+        '12': 'December'
+      }
+      var program_products= [
         'BCG doses',
         'BCG Diluent',
         'Hep.B doses',
@@ -30,110 +44,110 @@ angular.module('lmisChromeApp')
         'Auto Disable Syringes',
         '5mls Syringes',
         'Safety boxes'
-    ]
-    var addRecord={
+      ]
+      var addRecord={
         /**
-       * Add/Update Stock count
-       *
-       * @param {object} Stock Data.
-       * @return {Promise} return promise object
-       * @public
-       */
+         * Add/Update Stock count
+         *
+         * @param {object} Stock Data.
+         * @return {Promise} return promise object
+         * @public
+         */
         stock: function(object){
-            var deferred = $q.defer();
+          var deferred = $q.defer();
 
-            storageService.insert('stockCount', object).then(function(uuid){
-                deferred.resolve(uuid);
-            });
-            return deferred.promise;
+          storageService.insert('stockCount', object).then(function(uuid){
+            deferred.resolve(uuid);
+          });
+          return deferred.promise;
         },
-       /**
-       * Add/Update Stock wastage count
-       *
-       * @param {object} Stock wastage Data.
-       * @return {Promise} return promise object
-       */
+        /**
+         * Add/Update Stock wastage count
+         *
+         * @param {object} Stock wastage Data.
+         * @return {Promise} return promise object
+         */
         wastage: function(object){
-           var deferred = $q.defer();
+          var deferred = $q.defer();
 
-            storageService.insert('wastageCount', object).then(function(uuid){
-                console.log(object);
-                deferred.resolve(uuid);
-            });
-            return deferred.promise;
+          storageService.insert('wastageCount', object).then(function(uuid){
+            console.log(object);
+            deferred.resolve(uuid);
+          });
+          return deferred.promise;
         }
-    }
-    var openedProductCount = function(StockObject, facility, year, month, day, product){
-             var day = day < 10 ? '0' + day : day;
-             var key = facility+year+month+day;
-              //return key;
-             var row = StockObject[key];
-             if(Object.prototype.toString.call(row) === '[object Object]'){
-                  if(Object.keys(row).indexOf('used_opened') !== -1){
-                      if(Object.keys(row['used_opened']).indexOf(product.toString()) !== -1){
-                        return row['used_opened'][product.toString()]
-                      }
-                  }
-             }
-        }
-        var unOpenedProductCount = function(StockObject, facility, year, month, day, product){
-               var day = day < 10 ? '0' + day : day;
-                 var key = facility+year+month+day;
-                  //return key;
-                 var row = StockObject[key];
-                 if(Object.prototype.toString.call(row) === '[object Object]'){
-                      if(Object.keys(row).indexOf('used_unopened') !== -1){
-                          if(Object.keys(row['used_unopened']).indexOf(product.toString()) !== -1){
-                            return row['used_unopened'][product.toString()]
-                          }
-                      }
-                 }
+      }
+      var openedProductCount = function(StockObject, facility, year, month, day, product){
+        var day = day < 10 ? '0' + day : day;
+        var key = facility+year+month+day;
+        //return key;
+        var row = StockObject[key];
+        if(Object.prototype.toString.call(row) === '[object Object]'){
+          if(Object.keys(row).indexOf('used_opened') !== -1){
+            if(Object.keys(row['used_opened']).indexOf(product.toString()) !== -1){
+              return row['used_opened'][product.toString()]
             }
-    var load={
+          }
+        }
+      }
+      var unOpenedProductCount = function(StockObject, facility, year, month, day, product){
+        var day = day < 10 ? '0' + day : day;
+        var key = facility+year+month+day;
+        //return key;
+        var row = StockObject[key];
+        if(Object.prototype.toString.call(row) === '[object Object]'){
+          if(Object.keys(row).indexOf('used_unopened') !== -1){
+            if(Object.keys(row['used_unopened']).indexOf(product.toString()) !== -1){
+              return row['used_unopened'][product.toString()]
+            }
+          }
+        }
+      }
+      var load={
         allStockCount: function(){
-            var deferred = $q.defer();
-            storageService.all('stockCount')
-                .then(function(stockCount){
-                    deferred.resolve(stockCount);
-                });
-            return deferred.promise;
+          var deferred = $q.defer();
+          storageService.all('stockCount')
+              .then(function(stockCount){
+                deferred.resolve(stockCount);
+              });
+          return deferred.promise;
         },
         createStockObject: function(stockCount){
-            var stockObject = {};
-            for(var i in stockCount){
-                var key = stockCount[i].facility+stockCount[i].year.toString()+stockCount[i].month.toString()+stockCount[i].day.toString();
-                stockObject[key] = stockCount[i];
-            }
-            return stockObject;
+          var stockObject = {};
+          for(var i in stockCount){
+            var key = stockCount[i].facility+stockCount[i].year.toString()+stockCount[i].month.toString()+stockCount[i].day.toString();
+            stockObject[key] = stockCount[i];
+          }
+          return stockObject;
         },
 
         stockCountColumnData: function(programProducts, StockObject, facility, year, month, day){
 
-            var html = '<td>'+day+'</td>';
-            for(var i=0; i<programProducts.length; i++){
-                var opened = openedProductCount(StockObject, facility, year, month, day, i);
-                var unopened = unOpenedProductCount(StockObject, facility, year, month, day, i);
-                opened = angular.isUndefined(opened)?'':opened;
-                unopened = angular.isUndefined(unopened)?'':unopened;
-                html += '<td>'+opened+'</td>';
-                html += '<td>'+unopened+'</td>';
-            }
-            return html;
+          var html = '<td>'+day+'</td>';
+          for(var i=0; i<programProducts.length; i++){
+            var opened = openedProductCount(StockObject, facility, year, month, day, i);
+            var unopened = unOpenedProductCount(StockObject, facility, year, month, day, i);
+            opened = angular.isUndefined(opened)?'':opened;
+            unopened = angular.isUndefined(unopened)?'':unopened;
+            html += '<td>'+opened+'</td>';
+            html += '<td>'+unopened+'</td>';
+          }
+          return html;
         },
         stockCountRow: function(uuid){
-            storageService.get('stockCount', uuid)
-                .then(function(stockCount){
-                    deferred.resolve(stockCount);
-                });
-            return deferred.promise;
+          storageService.get('stockCount', uuid)
+              .then(function(stockCount){
+                deferred.resolve(stockCount);
+              });
+          return deferred.promise;
         },
         allWasteCount: function(){
-            var deferred = $q.defer();
-            storageService.all('wastageCount')
-                .then(function(wastageCount){
-                    deferred.resolve(wastageCount);
-                });
-            return deferred.promise;
+          var deferred = $q.defer();
+          storageService.all('wastageCount')
+              .then(function(wastageCount){
+                deferred.resolve(wastageCount);
+              });
+          return deferred.promise;
         },
         /*
          * load a single row from waste count table
@@ -141,12 +155,12 @@ angular.module('lmisChromeApp')
          * @return {Promise} return promise object
          */
         wasteCountRow: function(uuid){
-            var deferred = $q.defer();
-            storageService.get('wastageCount', uuid)
-                .then(function(wastageCount){
-                    deferred.resolve(wastageCount);
-                });
-            return deferred.promise;
+          var deferred = $q.defer();
+          storageService.get('wastageCount', uuid)
+              .then(function(wastageCount){
+                deferred.resolve(wastageCount);
+              });
+          return deferred.promise;
         },
         userFacilities: function(){
           /*
@@ -154,29 +168,30 @@ angular.module('lmisChromeApp')
            * @param {void}.
            * @return {Promise} return promise object
            */
-            var deferred = $q.defer();
-            var file_url = 'scripts/fixtures/user_related_facilities.json';
-            $http.get(file_url).success(function (data) {
-                deferred.resolve(data);
-            });
-            return deferred.promise;
+          var deferred = $q.defer();
+          var file_url = 'scripts/fixtures/user_related_facilities.json';
+          $http.get(file_url).success(function (data) {
+            deferred.resolve(data);
+          });
+          return deferred.promise;
         },
         locations: function(){
-            var deferred = $q.defer();
-            var file_url2 = 'scripts/fixtures/locations.json';
-            $http.get(file_url2).success(function (data){
-                deferred.resolve(data);
-            });
-            return deferred.promise;
+          var deferred = $q.defer();
+          var file_url2 = 'scripts/fixtures/locations.json';
+          $http.get(file_url2).success(function (data){
+            deferred.resolve(data);
+          });
+          return deferred.promise;
         }
 
-    }
+      }
 
 
-    return {
+      return {
         programProducts: program_products,
+        monthList: months,
         discardedReasons: discarded_reasons,
         save:addRecord,
         get:load
-    };
-  });
+      };
+    });
