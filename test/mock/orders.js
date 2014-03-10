c856b5d492f56d113311e711926d5305dc6da8c0
Tom Vincent <git@tlvince.com>
2014-03-10 11:38:02 +0000

    Add datestring format test

--- test/mock/orders.js
+++ test/mock/orders.js
@@ -4,7 +4,7 @@ angular.module('ordersMocks', [])
   // jshint camelcase: false
   .value('ordersMock', [
     {
-      created: '2014-03-04',
+      created: '2014-03-04T12:00:00Z',
       date_receipt: '2014-03-08',
       consumption: 10
     }
--- test/spec/services/inventoryrulesfactory.js
+++ test/spec/services/inventoryrulesfactory.js
@@ -15,7 +15,22 @@ describe('Service: inventoryRulesFactory', function() {
   it('should calculate the lead time for an order in ms', function() {
     var leadTime = inventoryRulesFactory.leadTime(orders[0]);
     expect(typeof leadTime).toBe('number');
-    expect(leadTime).toBe(345600000);
+    expect(leadTime).toBe(302400000);
+  });
+
+  it('should fail with non-compliant datestrings', function() {
+    // Expects RFC2822 or ISO 8601 dates
+    // See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+    var order = {
+      // Whitespace separator is invalid here; should be 'T'
+      created: '2014-03-04 12:30:30',
+      // Just the date component is valid ISO 8601
+      // jshint camelcase: false
+      date_receipt: '2014-03-08',
+    };
+
+    var leadTime = inventoryRulesFactory.leadTime(order);
+    expect(leadTime).toBeNaN();
   });
 
   it('should calculate a facility consumption level', function() {
