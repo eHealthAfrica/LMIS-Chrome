604a62dc607b34506bcd121df2066ee9f15bd423
Tom Vincent <git@tlvince.com>
2014-03-12 20:38:02 +0000

    Prefer function expressions
    See: http://stackoverflow.com/q/336859

--- app/scripts/services/facilityfactory.js
+++ app/scripts/services/facilityfactory.js
@@ -3,7 +3,7 @@
 angular.module('lmisChromeApp')
   .factory('facilityFactory', function($q, $rootScope, storageService) {
 
-    function getByUUID(uuid) {
+    var getByUUID = function(uuid) {
       var deferred = $q.defer();
       storageService.find(storageService.FACILITY, uuid).then(function(data) {
         var facility = data;
@@ -13,45 +13,46 @@ angular.module('lmisChromeApp')
         deferred.resolve(facility);
       });
       return deferred.promise;
-    }
+    };
+
+    var getFacilityInventory = function() {
+      var deferred = $q.defer(),
+        facilities = [];
+
+      storageService.all(storageService.FACILITY).then(function(data) {
+        angular.forEach(data, function(datum) {
+          if (datum !== undefined) {
+            facilities.push(getByUUID(datum.uuid).then(function(facility) {
+              deferred.notify(datum);
+              return facility;
+            }));
+          }
+        });
+
+        $q.all(facilities).then(function(results) {
+          deferred.resolve(results);
+          if (!$rootScope.$$phase) {
+            $rootScope.$apply();
+          }
+        });
+      });
+      return deferred.promise;
+    };
+
+    var getCurrentFacility = function() {
+      //TODO: replace with facility of current logged in user when we have the
+      //session and authentication working.
+      var deferred = $q.defer();
+      getByUUID('d48a39fb-6d37-4472-9983-bc0720403719').then(function(data) {
+        deferred.resolve(data);
+      });
+      return deferred.promise;
+    };
 
     // Public API here
     return {
-      getFacilityInventory: function() {
-        var deferred = $q.defer(),
-          facilities = [];
-
-        storageService.all(storageService.FACILITY).then(function(data) {
-          angular.forEach(data, function(datum) {
-            if (datum !== undefined) {
-              facilities.push(getByUUID(datum.uuid).then(function(facility) {
-                deferred.notify(datum);
-                return facility;
-              }));
-            }
-          });
-
-          $q.all(facilities).then(function(results) {
-            deferred.resolve(results);
-            if (!$rootScope.$$phase) {
-              $rootScope.$apply();
-            }
-          });
-        });
-        return deferred.promise;
-      },
-
+      getFacilityInventory: getFacilityInventory,
       get: getByUUID,
-
-      getCurrentFacility: function() {
-        //TODO: replace with facility of current logged in user when we have the
-        //session and authentication working.
-        var deferred = $q.defer();
-        getByUUID('d48a39fb-6d37-4472-9983-bc0720403719').then(function(data) {
-          deferred.resolve(data);
-        });
-        return deferred.promise;
-      }
-
+      getCurrentFacility: getCurrentFacility
     };
   });
