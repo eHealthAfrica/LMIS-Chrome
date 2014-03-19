1756cc277a4b611382477b375ac0caf1fc72a81b
Tom Vincent <git@tlvince.com>
2014-03-19 22:05:28 +0000

    Add charted values to summary table

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -85,11 +85,22 @@ angular.module('lmisChromeApp')
       templateUrl: 'views/home/dashboard.html',
       abstract: true,
       resolve: {
-        inventories: function(currentFacility, inventoryFactory) {
-          return inventoryFactory.getFacilityInventory(currentFacility.uuid);
-        },
         settings: function(settingsService) {
           return settingsService.load();
+        },
+        aggregatedInventory: function($q, $log, currentFacility, inventoryFactory, dashboardfactory, settings) {
+          var deferred = $q.defer();
+
+          inventoryFactory.getFacilityInventory(currentFacility.uuid)
+            .then(function(inventory) {
+              var values = dashboardfactory.aggregateInventory(inventory, settings);
+              deferred.resolve(values);
+            })
+            .catch(function(reason) {
+              $log.error(reason);
+            });
+
+          return deferred.promise;
         }
       },
       controller: function($scope, settings) {
@@ -100,24 +111,31 @@ angular.module('lmisChromeApp')
     })
     .state('home.index.dashboard.chart', {
       url: '',
+      resolve: {
+        keys: function(dashboardfactory) {
+          return dashboardfactory.keys();
+        }
+      },
       views: {
         'chart': {
           templateUrl: 'views/home/dashboard/chart.html',
-          controller: function($scope, inventories, settings, dashboardfactory, $log) {
-            var values = dashboardfactory.aggregateInventory(inventories, settings);
-            dashboardfactory.keys()
-              .then(function(keys) {
-                $scope.inventoryChart = dashboardfactory.chart(keys, values);
-              })
-              .catch(function(reason) {
-                $log.error(reason);
-              });
+          controller: function($scope, $log, dashboardfactory, keys, aggregatedInventory) {
+            $scope.inventoryChart = dashboardfactory.chart(keys, aggregatedInventory);
           }
         },
         'table': {
           templateUrl: 'views/home/dashboard/table.html',
-          controller: function($scope, settings) {
-            $scope.products = settings.inventory.products;
+          controller: function($scope, settings, aggregatedInventory) {
+            var products = settings.inventory.products;
+
+            // Get the service level for use in view
+            var serviceLevel = 0;
+            for(var i = aggregatedInventory.length - 1; i >= 0; i--) {
+              serviceLevel = products[aggregatedInventory[i].label].serviceLevel;
+              aggregatedInventory[i].serviceLevel = serviceLevel;
+            }
+
+            $scope.products = aggregatedInventory;
           }
         }
       }
