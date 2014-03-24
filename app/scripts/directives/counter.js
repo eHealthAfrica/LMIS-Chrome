02e28d8f82adee5c203f60e735832659ebaad87b
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-03-24 16:54:44 +0100

    moved counter html to counter.js

--- app/scripts/directives/counter.js
+++ app/scripts/directives/counter.js
@@ -2,7 +2,19 @@ angular.module('lmisChromeApp').directive('counter', function () {
   return {
     restrict: 'E',
     transclude: true,
-    templateUrl: 'views/partials/counter-ui.html',
+    template: '<div class="input-group">' +
+        '<span class="input-group-btn">' +
+        '<button class="btn btn-warning btn-lg" type="button" ng-click="count = _tapInputSub(count)">' +
+        '<i class="fa fa-minus"></i>' +
+        '</button>' +
+        '</span>' +
+        '<input type="text" class="form-control input-lg" size="20" ng-model="count" required>' +
+        '<span class="input-group-btn">' +
+        '<button class="btn btn-info btn-lg" type="button" ng-click="count = _tapInputAdd(count)">' +
+        '<i class="fa fa-plus"></i>' +
+        '</button>' +
+        '</span>' +
+        '</div>',
     scope: {
       count: '=bind'
     },
