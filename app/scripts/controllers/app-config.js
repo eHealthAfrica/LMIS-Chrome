b1cf5ab486f97ede8782c9458415e80a52f7bfc6
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-03-26 14:37:18 +0100

    updated app-config controller  Refs item: 276

--- app/scripts/controllers/app-config.js
+++ app/scripts/controllers/app-config.js
@@ -7,15 +7,46 @@ angular.module('lmisChromeApp').config(function ($stateProvider) {
     resolve: {
       facilities: function (facilityFactory) {
         return facilityFactory.getAll();
+      },
+      productProfiles: function(productProfileFactory){
+        return productProfileFactory.getAll();
       }
     },
     controller: 'AppConfigCtrl',
     data: {
-      label: "App Configuration"
+      label: 'App Configuration'
     }
   })
-}).controller('AppConfigCtrl', function ($scope, facilities, appConfigService) {
-      $scope.stockCountIntervals = appConfigService.stockCountIntervals;
-      $scope.facilities = facilities;
-      $scope.appConfig = {};//used to hold config form data
+}).controller('AppConfigCtrl', function ($scope, facilities, productProfiles, appConfigService) {
+ $scope.stockCountIntervals = appConfigService.stockCountIntervals;
+ $scope.facilities = facilities;
+ //used to hold config form data
+ $scope.appConfig = {
+   facility: '',
+   stockCountInterval: '',
+   contactPerson: {
+     name: '',
+     email: '',
+     phoneNo: ''
+   },
+   selectedProductProfiles: []
+ };
+ $scope.productProfileCheckBoxes = [];
+ $scope.productProfiles = productProfiles;
+
+ $scope.handleSelectionEvent = function(selection){
+   var productProfile = JSON.parse(selection);
+   if(productProfile.deSelected === undefined){
+     $scope.appConfig.selectedProductProfiles.push(productProfile);
+     return;
+   }
+   $scope.appConfig.selectedProductProfiles = $scope.appConfig.selectedProductProfiles
+    .filter(function (prodProf) {
+      return prodProf.uuid !== productProfile.uuid;
+   });
+ };
+
+ $scope.save = function(){
+   console.log($scope.appConfig);
+ };
 });
