c4e6dc2c1ae1f7bd663b236d8d7d587317259584
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-03-27 11:39:19 +0100

    added functions to appConfigService that will enable loading and configuration of app config

--- app/scripts/services/app-config-service.js
+++ app/scripts/services/app-config-service.js
@@ -1,6 +1,6 @@
 'use strict'
 
-angular.module('lmisChromeApp').service('appConfigService', function (storageService) {
+angular.module('lmisChromeApp').service('appConfigService', function ($q, storageService) {
 
   this.stockCountIntervals = [
     {name: 'Daily', value: 1},
@@ -9,14 +9,64 @@ angular.module('lmisChromeApp').service('appConfigService', function (storageSer
     {name: 'Monthly', value: 30}
   ];
 
+  var createAppConfig = function (appConfig) {
+    var deferred = $q.defer();
+    storageService.insert(storageService.APP_CONFIG, appConfig).then(function (insertionResult) {
+      deferred.resolve(insertionResult);
+    }, function (reason) {
+      deferred.reject(reason);
+    });
+    return deferred.promise;
+  }
+
+  /**
+   * This function setups or configures the app, it checks if a configuration exist then over-writes it, else,
+   * it creates a new configuration.
+   *
+   * @param appConfig
+   * @returns {promise|promise|*|Function|promise}
+   */
   this.setup = function (appConfig) {
-    console.log(appConfig);
+    var deferred = $q.defer();
+    this.load().then(function (result) {
+      if (result === undefined) {
+        //app config does not exist already
+        createAppConfig(appConfig).then(function (result) {
+          deferred.resolve(result);
+        }, function (reason) {
+          deferred.reject(reason);
+        });
+
+      } else {
+        //app config exist, overwrite
+        var storedAppConfigUUID = Object.keys(result)[0];//get key of the first and only app config
+        appConfig['uuid'] = storedAppConfigUUID;//over-write appConfig by using same uuid.
+        createAppConfig(appConfig).then(function (result) {
+          deferred.resolve(result);
+        }, function (reason) {
+          deferred.reject(reason);
+        });
+      }
+    });
+    return deferred.promise;
   };
 
-  this.load = function(){
-    storageService.all(storageService.APP_CONFIG).then(function(){
-
+  this.load = function () {
+    var deferred = $q.defer();
+    storageService.get(storageService.APP_CONFIG).then(function (data) {
+      if (data === undefined) {
+        deferred.resolve(data);
+        return;
+      }
+      if (Object.keys(data).length === 1) {
+        deferred.resolve(data);
+      } else {
+        throw 'there are more than one app config on this app.';
+      }
+    }, function (error) {
+      deferred.reject(error);
     });
+    return deferred.promise;
   };
 
 });
