096f9b665104df061c749e166cd7df99f85326c9
Musa Musa <musakunte@gmail.com>
2014-03-28 14:58:26 +0100

    added logic for waste count validation and saving to database

--- app/scripts/controllers/stockcount.js
+++ app/scripts/controllers/stockcount.js
@@ -42,6 +42,9 @@ angular.module('lmisChromeApp')
         resolve: {
           currentFacility: function(facilityFactory){
             return facilityFactory.getCurrentFacility();
+          },
+          productType: function(stockCountFactory){
+            return stockCountFactory.productType();
           }
         }
       })
@@ -107,9 +110,156 @@ angular.module('lmisChromeApp')
 /*
  * Wastage Count Controller
  */
-  .controller('WasteCountFormCtrl', function($scope, stockCountFactory, $state){
+  .controller('WasteCountFormCtrl', function($scope, stockCountFactory, alertsFactory, $stateParams, currentFacility,$state, productType, $log, $translate, pouchdb, config){
+
+    var now = new Date();
+    var day = now.getDate();
+    day = day < 10 ? '0' + day : day;
+
+    var month = now.getMonth() + 1;
+    month = month < 10 ? '0' + month : month;
+    $scope.productType = productType;
+    $scope.discardedReasons = stockCountFactory.discardedReasons;
+
+    $scope.step = 0;
+    $scope.monthList = stockCountFactory.monthList;
+    /*
+     * get url parameters
+     */
+    $scope.facilityObject = currentFacility;
+    $scope.facilityUuid = ($stateParams.facility !== null)?$stateParams.facility:$scope.facilityObject.uuid;
+    $scope.reportMonth = ($stateParams.reportMonth !== null)?$stateParams.reportMonth:month;
+    $scope.reportYear = ($stateParams.reportYear !== null)?$stateParams.reportYear: now.getFullYear();
+    $scope.currentDay = day;
+
+    $scope.wasteCount = {};
+    $scope.wasteCount.discarded = {};
+    $scope.wasteCount.reason = {};
+
+    $scope.wasteCount.countDate ='';
+
+    $scope.facilityProducts = stockCountFactory.facilityProducts(); // selected products for current facility
+    $scope.facilityProductsKeys = Object.keys($scope.facilityProducts); //facility products uuid list
+    $scope.productKey = $scope.facilityProductsKeys[$scope.step];
+    for(var i=0; i<$scope.facilityProductsKeys.length; i++){
+      $scope.wasteCount.reason[$scope.facilityProductsKeys[i]]={};
+    }
+    for(var i in  $scope.discardedReasons){
+      $scope.wasteCount.reason[$scope.facilityProductsKeys[$scope.step]][i]={};
+    }
+    //set maximum steps
+    if($scope.facilityProductsKeys.length>0){
+      $scope.maxStep =  $scope.facilityProductsKeys.length-1;
+    }
+    else{
+      $scope.maxStep =0;
+    }
+
+    $scope.edit = function(index){
+      $scope.step = index;
+      $scope.productKey = $scope.facilityProductsKeys[$scope.step];
+      $scope.preview = false;
+      $scope.editOn = true;
+    };
+
+    $scope.selectedFacility = stockCountFactory.get.productReadableName($scope.facilityProducts, $scope.step);
+    $scope.productTypeCode = stockCountFactory.get.productTypeCode($scope.facilityProducts, $scope.step, $scope.productType);
+    $scope.redirect = true; //initialize redirect as true
+    $scope.wasteCount.isComplete = 1; //and stock count entry as completed
+    $scope.wasteCount.lastPosition = 0;
+    var timezone = stockCountFactory.get.timezone();
+
+    //load existing count for the day if any.
+    var date = $scope.reportYear+'-'+$scope.reportMonth+'-'+$scope.currentDay;
+    stockCountFactory.getWasteCountByDate(date).then(function(wasteCount){
+      if(wasteCount !== null){
+        $scope.wasteCount = wasteCount;
+        $scope.editOn = true; // enable edit mode
+      }
+    });
+
+    $scope.save = function(){
+      var dbName = 'wastecount';
+      $scope.wasteCount.facility = $scope.facilityUuid;
+      $scope.wasteCount.countDate = new Date($scope.reportYear, parseInt($scope.reportMonth)-1, $scope.currentDay, timezone);
+
+      stockCountFactory.save.waste($scope.wasteCount)
+        .then(function() {
+          if($scope.redirect) {
+            $translate('stockCountSaved')
+              .then(function(stockCountSaved) {
+                alertsFactory.success(stockCountSaved);
+              })
+              .then(function() {
+                var db = pouchdb.create(name);
+                var obj = $scope.wasteCount;
+                obj._id = obj.uuid;
+                db.put(obj)
+                  .then(function() {
+                    var cb = {complete: function() {
+                      $translate('syncSuccess')
+                        .then(function(syncSuccess) {
+                          alertsFactory.success(syncSuccess);
+                        })
+                        .then(function() {
+                          if($scope.redirect) {
+                            var msg = [
+                              'You have completed waste count for',
+                              $scope.currentDay,
+                              $scope.monthList[$scope.reportMonth],
+                              $scope.reportYear
+                            ];
+                            alertsFactory.success(msg.join(' '));
+                            $scope.go('home.index.mainActivity', {
+                              'facility': $scope.facilityUuid,
+                              'reportMonth': $scope.reportMonth,
+                              'reportYear': $scope.reportYear,
+                              'stockResult': msg
+                            });
+                          }
+                        })
+                        .catch(function(reason) {
+                          $log.error(reason);
+                        });
+                    }};
+                    var db = pouchdb.create(name);
+                    db.replicate.to(config.apiBaseURI + '/' + dbName, cb);
+                  })
+                  .catch(function(reason) {
+                    if(reason.message) {
+                      alertsFactory.danger(reason.message);
+                    }
+                    $log.error(reason);
+                  });
+              });
+          }
+          $scope.redirect = true; // always reset to true after every save
+          $scope.wasteCount.isComplete = 1;
+        });
+    };
 
 
+    $scope.changeState = function(direction){
+      $scope.currentEntry = $scope.wasteCount.discarded[$scope.facilityProductsKeys[$scope.step]];
+      if(stockCountFactory.validate.invalid($scope.currentEntry) && direction !== 0){
+        alertsFactory.danger($scope.alertMsg);
+      }
+      else{
+        if(direction !== 2){
+          $scope.step = direction === 0? $scope.step-1 : $scope.step + 1;
+        }
+        else{
+          $scope.preview = true;
+        }
+        $scope.productKey = $scope.facilityProductsKeys[$scope.step];
+        //TODO: this is best done with $timeout to auto save data when interface is idle for x mount of time
+        $scope.redirect = false;// we don't need to redirect when this fn calls save()
+        $scope.wasteCount.isComplete = 0;// when saved from this fn its not complete yet
+        $scope.save();
+      }
+      $scope.selectedFacility = stockCountFactory.get.productReadableName($scope.facilityProducts, $scope.step);
+      $scope.productTypeCode = stockCountFactory.get.productTypeCode($scope.facilityProducts, $scope.step, $scope.productType);
+    };
 
   })
   .controller('StockCountStepsFormCtrl', function($scope, stockCountFactory, $state, alertsFactory, $stateParams, currentFacility, productType, $log, $translate, pouchdb, config){
@@ -138,9 +288,7 @@ angular.module('lmisChromeApp')
 
 
     $scope.stockCount = {};
-    $scope.stockCount.opened = {};
     $scope.stockCount.unopened = {};
-    $scope.stockCount.confirmation = {};
 
     $scope.stockCount.countDate = '';
     $scope.alertMsg = 'stock count value is invalid, at least enter Zero "0" to proceed';
