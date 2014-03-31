0a43153229ae0ad6c8d5d63561e6e744e9e877f0
Tom Vincent <git@tlvince.com>
2014-03-31 22:59:54 +0100

    Beautify app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,24 +1,23 @@
 'use strict';
 
 angular.module('lmisChromeApp', [
-      'ngResource',
-      'ngSanitize',
-      'ngCookies',
-      'ui.bootstrap',
-      'ui.router',
-      'tv.breadcrumbs',
-      'toggle-switch',
-      'pouchdb',
-      'config'
-    ])
-
+  'ngResource',
+  'ngSanitize',
+  'ngCookies',
+  'ui.bootstrap',
+  'ui.router',
+  'tv.breadcrumbs',
+  'toggle-switch',
+  'pouchdb',
+  'config'
+])
   // Disable ui-router auto scrolling
-    .config(function ($uiViewScrollProvider, $anchorScrollProvider) {
-      $uiViewScrollProvider.useAnchorScroll();
-      $anchorScrollProvider.disableAutoScrolling();
-    })
+  .config(function($uiViewScrollProvider, $anchorScrollProvider) {
+    $uiViewScrollProvider.useAnchorScroll();
+    $anchorScrollProvider.disableAutoScrolling();
+  })
 
-  // Central Variable for Watching Online/Offline Events
-    .run(function(storageService) {
-      storageService.loadFixtures();
-    });
+  // Load fixture data
+  .run(function(storageService) {
+    storageService.loadFixtures();
+  });
