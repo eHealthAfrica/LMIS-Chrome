a4c5f69c6423499288c27e8eeff4126668d1167a
Tom Vincent <git@tlvince.com>
2014-03-31 22:13:39 +0100

    Modularise root state

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,23 +19,6 @@ angular.module('lmisChromeApp', [
     })
 
   // Central Variable for Watching Online/Offline Events
-    .run(function ($window, $rootScope, $state, $stateParams, storageService) {
+    .run(function(storageService) {
       storageService.loadFixtures();
-
-      $rootScope.online = navigator.onLine;
-
-      $window.addEventListener('offline', function () {
-        $rootScope.$apply(function () {
-          $rootScope.online = false;
-        });
-      }, false);
-
-      $window.addEventListener('online', function () {
-        $rootScope.$apply(function () {
-          $rootScope.online = true;
-        });
-      }, false);
-
-      // Convenience property to get the current state
-      $rootScope.$state = $state;
     });
