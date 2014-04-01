84dae2aa8c7e0452ba4994b8dfe8a4f2d8c928da
Tom Vincent <git@tlvince.com>
2014-04-01 12:08:15 +0100

    Don't copy bower_components in build
    Fixed upstream:
    https://github.com/yeoman/generator-angular/commit/4c53ea975d3a776c416e078389a2657ec4e56ac0

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,7 +252,6 @@ module.exports = function(grunt) {
             '.htaccess',
             '*.html',
             'views/**/*.html',
-            'bower_components/**/*',
             'images/{,*/}*.{webp}',
             'fonts/*'
           ]
