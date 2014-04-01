078b5a38b3322808be08ce13da4803e289c3da79
Tom Vincent <git@tlvince.com>
2014-04-01 12:24:03 +0100

    Handle font-awesome fonts in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -242,25 +242,33 @@ module.exports = function(grunt) {
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= yeoman.app %>',
-          dest: '<%= yeoman.dist %>',
-          src: [
-            '*.{ico,png,txt}',
-            '.htaccess',
-            '*.html',
-            'views/**/*.html',
-            'images/{,*/}*.{webp}',
-            'fonts/*'
-          ]
-        }, {
-          expand: true,
-          cwd: '.tmp/images',
-          dest: '<%= yeoman.dist %>/images',
-          src: ['generated/*']
-        }]
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: '<%= yeoman.app %>',
+            dest: '<%= yeoman.dist %>',
+            src: [
+              '*.{ico,png,txt}',
+              '.htaccess',
+              '*.html',
+              'views/**/*.html',
+              'images/{,*/}*.{webp}',
+            ]
+          },
+          {
+            expand: true,
+            cwd: '.tmp/images',
+            dest: '<%= yeoman.dist %>/images',
+            src: ['generated/*']
+          },
+          {
+            expand: true,
+            cwd: '<%= yeoman.app %>/bower_components/font-awesome',
+            dest: '<%= yeoman.dist %>',
+            src: ['fonts/*']
+          }
+        ]
       },
       styles: {
         expand: true,
