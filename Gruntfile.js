48d7dca588d0aa97f111bdee6fa5acc30844e27a
Tom Vincent <git@tlvince.com>
2014-04-01 12:07:05 +0100

    Recursively copy views
    We have views more than two-levels deep, so switch to a recursive glob

--- Gruntfile.js
+++ Gruntfile.js
@@ -251,7 +251,7 @@ module.exports = function(grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
-            'views/{,*/}*.html',
+            'views/**/*.html',
             'bower_components/**/*',
             'images/{,*/}*.{webp}',
             'fonts/*'
