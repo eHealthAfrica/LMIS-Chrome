5009291675c9a3836c92d03c90ec886e367b14a1
Tom Vincent <git@tlvince.com>
2014-04-01 15:28:02 +0100

    Remove CDNify task
    This is a Chrome packaged app and therefore cannot use external scripts (without
    sandboxing).

--- Gruntfile.js
+++ Gruntfile.js
@@ -239,13 +239,6 @@ module.exports = function(grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -408,7 +401,6 @@ module.exports = function(grunt) {
     'concat',
     'ngmin',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'rev',
