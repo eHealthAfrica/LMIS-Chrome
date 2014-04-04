0fb826b6466802136d567d530cf8e8f30ca64c5f
Vitaly <motor.kodlife@gmail.com>
2014-04-04 08:49:38 -0500

    add options object to #get() to define whether return an item or collection

--- app/scripts/services/storage-service.js
+++ app/scripts/services/storage-service.js
@@ -92,7 +92,7 @@ angular.module('lmisChromeApp')
        */
 
       function getData(key) {
-         var promise = chromeStorageApi.get(key, false);
+         var promise = chromeStorageApi.get(key);
          return promise;
         }
 
@@ -104,7 +104,7 @@ angular.module('lmisChromeApp')
        */
         // TODO - consider to deprecate
         function getAllFromStore() {
-          var promise = chromeStorageApi.get(null, true);
+          var promise = chromeStorageApi.get(null, {collection:true});
           return promise;
         }
 
