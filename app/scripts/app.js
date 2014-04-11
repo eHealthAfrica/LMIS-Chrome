caf11cb019fa858da7202c9a0a6fdbea0d0083e6
Tom Vincent <git@tlvince.com>
2014-04-11 15:02:02 +0100

    Adds basic stock count graph, see item:378

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,12 +5,13 @@ angular.module('lmisChromeApp', [
   'ui.router',
   'tv.breadcrumbs',
   'pouchdb',
-  'config'
+  'config',
+  'nvd3ChartDirectives'
 ])
   // Load fixture data
   .run(function(storageService) {
     storageService.getAll().then(function(data){
-      if(typeof data === 'undefined' || Object.keys(data).length == 0) 
+      if(typeof data === 'undefined' || Object.keys(data).length == 0)
         storageService.loadFixtures().then(function(result){
           storageService.getAll().then(function(data){
             console.log("finished loading: "+Object.keys(data));
