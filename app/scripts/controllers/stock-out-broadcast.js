e30fe61eb929c8559d2adc3fa2b6ea05d8bf46aa
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-04-12 11:40:00 +0100

    fixed beep and vibration using phonegap plugins

--- app/scripts/controllers/stock-out-broadcast.js
+++ app/scripts/controllers/stock-out-broadcast.js
@@ -37,57 +37,66 @@ angular.module('lmisChromeApp').config(function ($stateProvider) {
       facility: $scope.stockOutForm.facility
     };
 
-    var modal = $modal.open({
-      templateUrl: 'views/stock-out-broadcast/partials/confirm-stock-out-broadcast.html',
-      backdrop : 'static',
-      keyboard: false,
-      resolve: {
-        productType: function($q){
-          var deferred = $q.defer();
-          deferred.resolve(stockOut.productType);
-          return deferred.promise;
-        }
-      },
-      controller: function($scope, $state, $modalInstance, i18n, productType){
-        $scope.headerMessage = i18n('confirmStockOutHeader', productType.code);
-        $scope.bodyMessage = i18n('confirmStockOutBodyMsg');
-        $scope.confirmBtnMsg = i18n('yes');
-        $scope.cancelBtnMsg = i18n('no');
-
-        $scope.confirm = function(){
-          var broadcastStockOutConfirmed = true;
-          $modalInstance.close(broadcastStockOutConfirmed);
-        };
-
-        $scope.cancel = function(){
-          $modalInstance.dismiss('you cancelled stock out broadcast');
-        }
-
-      }
-    });
-
-    modal.result.then(function (result) {
-      if(result === true){
-        stockOutBroadcastFactory.save(stockOut).then(function (result) {
-          //TODO: send SMS if offline
-          if (result !== undefined) {
-            stockOut.uuid = result;
-            stockOutBroadcastFactory.broadcast(stockOut)
-              .then(function(result){
-                $log.info('stock-out broad-casted');
-              }, function(reason){
-                $log.error(reason);
-            })
-            $state.go('home.index.mainActivity', {'stockOutBroadcastResult': true });
+    if(navigator.notification){
+      console.log('confirm from phonegap');
+       navigator.notification.confirm('Are you sure?', function(){
+         console.log('phone gap - stock out alert confirm');
+       }, 'Broadcast Stock-out Alert', 'Yes,No');
+    }else{
+      var modal = $modal.open({
+        templateUrl: 'views/stock-out-broadcast/partials/confirm-stock-out-broadcast.html',
+        backdrop: 'static',
+        keyboard: false,
+        resolve: {
+          productType: function ($q) {
+            var deferred = $q.defer();
+            deferred.resolve(stockOut.productType);
+            return deferred.promise;
           }
-        }, function (reason) {
-          alertsFactory.danger(i18n('stockOutBroadcastFailedMsg'));
-          $log.error(reason);
-        });
-      }
-    }, function (reason) {
-      $log.info(reason);
-    });
+        },
+        controller: function ($scope, $state, $modalInstance, i18n, productType) {
+          $scope.headerMessage = i18n('confirmStockOutHeader', productType.code);
+          $scope.bodyMessage = i18n('confirmStockOutBodyMsg');
+          $scope.confirmBtnMsg = i18n('yes');
+          $scope.cancelBtnMsg = i18n('no');
+
+          $scope.confirm = function () {
+            var broadcastStockOutConfirmed = true;
+            $modalInstance.close(broadcastStockOutConfirmed);
+          };
+
+          $scope.cancel = function () {
+            $modalInstance.dismiss('you cancelled stock out broadcast');
+          }
+
+        }
+      });
+
+      modal.result.then(function (result) {
+        if (result === true) {
+          stockOutBroadcastFactory.save(stockOut).then(function (result) {
+            //TODO: send SMS if offline
+            if (result !== undefined) {
+              stockOut.uuid = result;
+              stockOutBroadcastFactory.broadcast(stockOut)
+                .then(function (result) {
+                  $log.info('stock-out broad-casted');
+                }, function (reason) {
+                  $log.error(reason);
+              })
+              $state.go('home.index.mainActivity', {'stockOutBroadcastResult': true });
+            }
+          }, function (reason) {
+            alertsFactory.danger(i18n('stockOutBroadcastFailedMsg'));
+            $log.error(reason);
+          });
+        }
+      }, function (reason) {
+        $log.info(reason);
+      });
+
+    }
 
   };
+
 });
