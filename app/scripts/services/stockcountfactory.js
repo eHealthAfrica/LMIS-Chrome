c8ed53399e3452b47a49cc1a8d23e575723e1cd4
Musa Musa <musakunte@gmail.com>
2014-04-17 18:52:03 +0100

    added function to return week number for current and date validation refs item:397

--- app/scripts/services/stockcountfactory.js
+++ app/scripts/services/stockcountfactory.js
@@ -36,6 +36,17 @@ angular.module('lmisChromeApp')
       });
       return deferred.promise;
     };
+    var getWeek = function (dateObject) {
+      var date = new Date(dateObject.getTime());
+       date.setHours(0, 0, 0, 0);
+      // Thursday in current week decides the year.
+      date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+      // January 4 is always in week 1.
+      var week1 = new Date(date.getFullYear(), 0, 4);
+      // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+      return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                            - 3 + (week1.getDay() + 6) % 7) / 7);
+    }
     var addRecord={
       /**
        * Add/Update Stock count
@@ -452,7 +463,16 @@ angular.module('lmisChromeApp')
           var dayDifference = 1000 * 60 * 60 * 24 * (Math.abs(current.getDay() - scope.reminderDay))
           current = new Date(current.getTime() + (dayDifference));
         }
-
+        var today = $filter('date')(new Date().toJSON(), 'yyyy-MM-dd');
+        var appDate = $filter('date')(scope.dateActivated, 'yyyy-MM-dd');
+        var adjustedDate = $filter('date')(current.toJSON(), 'yyyy-MM-dd');
+        if( appDate === today || adjustedDate > today ){
+          var lastCountDate = new Date(current-interval);
+          if(getWeek(lastCountDate) === getWeek(new Date())){
+            dates.push(lastCountDate);
+          }
+          return dates;
+        }
         while(dates.length < scope.maxList){
           dates.push(current);
 
