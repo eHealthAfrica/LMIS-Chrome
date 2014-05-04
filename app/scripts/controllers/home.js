a254a3de1072bc50035612fd18078f83499f9db6
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-05-04 13:40:06 +0100

    added scrollToTop to home. mainActivity so that alerts will be visible refs item:435

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -69,20 +69,23 @@ angular.module('lmisChromeApp')
       views: {
         'activities': {
           templateUrl: 'views/home/main-activity.html',
-          controller: function ($scope, $stateParams, $log, $state, appConfig, i18n, alertsFactory) {
+          controller: function ($scope, $stateParams, $log, $state, appConfig, i18n, alertsFactory, utility) {
 
             if ($stateParams.storageClear !== null) {
+              utility.scrollToTop();
               alertsFactory.success(i18n('clearStorageMsg'));
               $stateParams.storageClear = null;
             }
 
             if ($stateParams.stockOutBroadcastResult !== null) {
+              utility.scrollToTop();
               alertsFactory.success(i18n('stockOutBroadcastSuccessMsg'));
               $stateParams.stockOutBroadcastResult = null;
             }
 
 
             if ($stateParams.appConfigResult !== null) {
+              utility.scrollToTop();
               alertsFactory.success($stateParams.appConfigResult);
               $stateParams.appConfigResult = null;
             }
@@ -93,6 +96,7 @@ angular.module('lmisChromeApp')
             }
 
             if ($stateParams.surveySuccessMsg !== null) {
+              utility.scrollToTop();
               alertsFactory.success($stateParams.surveySuccessMsg);
               $stateParams.surveySuccessMsg = null;
             }
