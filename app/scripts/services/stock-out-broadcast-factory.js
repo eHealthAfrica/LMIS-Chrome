95ecf62735a65964658c6ca1917697eaca15576c
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-05-27 14:18:11 +0100

    updated stock out broadcast to send sms after attempting to sync and syncing failed! Refs item:458

--- app/scripts/services/stock-out-broadcast-factory.js
+++ app/scripts/services/stock-out-broadcast-factory.js
@@ -14,42 +14,53 @@ angular.module('lmisChromeApp').factory('stockOutBroadcastFactory', function (st
     return deferred.promise;
   };
 
+  /**
+   * This function tries to sync stock-out alert if it fails, it sends sms alert.
+   * NB: syncItem updates pending sync record if sync fails #see syncService.syncItem for more detail.
+   *
+   * @param stockOut
+   * @returns {promise|Function|promise|promise|promise|*}
+   */
   var broadcastStockOut = function (stockOut) {
-
-    if ($window.navigator.onLine) {
-      return syncService.syncItem(storageService.STOCK_OUT, stockOut);
-    } else {
-      var msg = 'stkOut:'+stockOut.uuid+';facility:'+stockOut.facility.uuid+';prodType:'+stockOut.productType.uuid;
-      var smsPromise = notificationService.sendSms(notificationService.alertRecipient, msg);
-      var pendingSyncRecord = { dbName: storageService.STOCK_OUT, uuid: stockOut.uuid };
-      smsPromise.finally(function(){
-        //update pending sync record in the background
-        syncService.addToPendingSync(pendingSyncRecord);
-      });
-      return smsPromise;
-    }
-
-  };
-
-  var saveMultipleStockOut = function(stockOutList){
     var deferred = $q.defer();
-    storageService.insertBatch(storageService.STOCK_OUT, stockOutList)
-      .then(function(result){
-        deferred.resolve(result);
-      })
-      .catch(function(reason){
-        deferred.reject(reason);
-      });
+    syncService.syncItem(storageService.STOCK_OUT, stockOut).
+        then(function (result) {
+          deferred.resolve(result);
+        })
+        .catch(function (reason) {
+          //sync failed send sms alert
+          var msg = 'stkOut:' + stockOut.uuid + ';facility:' + stockOut.facility.uuid + ';prodType:' +
+              stockOut.productType.uuid;
+          notificationService.sendSms(notificationService.alertRecipient, msg)
+              .then(function (result) {
+                deferred.resolve(result);
+              })
+              .catch(function (reason) {
+                deferred.reject(reason);
+              });
+        });
     return deferred.promise;
   };
 
-  var getStockOut = function(){
+  var saveMultipleStockOut = function (stockOutList) {
+    var deferred = $q.defer();
+    storageService.insertBatch(storageService.STOCK_OUT, stockOutList)
+        .then(function (result) {
+          deferred.resolve(result);
+        })
+        .catch(function (reason) {
+          deferred.reject(reason);
+        });
+    return deferred.promise;
+  };
+
+  var getStockOut = function () {
     var deferred = $q.defer();
     storageService.all(storageService.STOCK_OUT)
         .then(function (result) {
           deferred.resolve(result);
         })
-        .catch(function(reason){
+        .catch(function (reason) {
           deferred.reject(reason);
         });
     return deferred.promise;
