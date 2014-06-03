2e8b1743f44d202056c3c7a1a584205ace85d6cc
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-06-03 17:12:14 +0100

    added a function that returns categorized list of ccu profiles refs item:470

--- app/scripts/services/ccu-profile-factory.js
+++ app/scripts/services/ccu-profile-factory.js
@@ -32,7 +32,7 @@ angular.module('lmisChromeApp').factory('ccuProfileFactory', function ($q, stora
     var groupedList = {};
     storageService.all(storageService.CCU_PROFILE)
         .then(function (result) {
-          for (var index in result) {
+          for (var index in result) {tem
             var ccuProfile = result[index];
             var NOT_FOUND = -1;
             var existingGroups = Object.keys(groupedList);
