843c2a3198e62faf79815ef043919ba2d1d3db2a
Tom Vincent <git@tlvince.com>
2014-06-17 16:31:04 +0100

    Trim whitespace on ga-click

--- app/scripts/directives/analytics.js
+++ app/scripts/directives/analytics.js
@@ -7,7 +7,7 @@ angular.module('lmisChromeApp')
       link: function(scope, element, attr) {
         var tracker = trackingFactory.tracker;
         element.on('click', function() {
-          tracker.sendEvent('Click', element.text(), attr.gaClick);
+          tracker.sendEvent('Click', element.text().trim(), attr.gaClick);
         });
       }
     };
