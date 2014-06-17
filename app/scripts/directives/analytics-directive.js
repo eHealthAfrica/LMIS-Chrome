ec56ec59b23e164b53d64c6545af8c45bbe72880
Tom Vincent <git@tlvince.com>
2014-06-17 16:15:45 +0100

    Simplify analytics page view reports
    Instead of annotating each view, we can leverage UI Router's [state change
    events](https://github.com/angular-ui/ui-router/wiki#state-change-events) to
    track which views are viewed.

--- app/scripts/directives/analytics-directive.js
+++ app/scripts/directives/analytics-directive.js
@@ -5,17 +5,10 @@ angular.module('lmisChromeApp')
     return {
       restrict: 'AE',
       link: function(scope, element, attr) {
-        var tracker = trackingFactory.tracker();
+        var tracker = trackingFactory.tracker;
         element.on('click', function() {
           tracker.sendEvent('Click', element.text(), attr.lmisPageViewReport);
         });
       }
-    };
-  })
-
-  .directive('lmisPageViewReport', function(trackingFactory) {
-    return function(scope, element, attr) {
-      var tracker = trackingFactory.tracker();
-      tracker.sendAppView(attr.lmisPageViewReport);
     };
   });
--- app/scripts/services/tracking-factory.js
+++ app/scripts/services/tracking-factory.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .factory('trackingFactory', function($window, config) {
+  .factory('trackingFactory', function($window, $rootScope, config) {
+
+    var tracker;
+    if ('analytics' in $window) {
+      var service = $window.analytics.getService(config.analytics.service);
+      tracker = service.getTracker(config.analytics.propertyID);
+
+      $rootScope.$on('$stateChangeSuccess', function(event, state) {
+        tracker.sendAppView(state.name);
+      });
+
+      $rootScope.$on('$stateNotFound', function(event, state) {
+        tracker.sendException(state.to, false);
+      });
+    }
+
     return {
-      tracker: function() {
-        if ('analytics' in $window) {
-          var service = $window.analytics.getService(config.analytics.service);
-          return service.getTracker(config.analytics.propertyID);
-        }
-      }
+      tracker: tracker
     };
   });
