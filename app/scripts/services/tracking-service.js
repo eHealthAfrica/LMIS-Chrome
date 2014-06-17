904f8a05021ab4b04267362aa3b2adbd79d0d730
thanasio <a.anthopoulos@ucl.ac.uk>
2014-06-17 13:03:17 +0100

    Merge remote-tracking branch 'upstream/develop' into feature/analytics
    Conflicts:
    	app/views/index/footer.html

--- app/scripts/services/tracking-service.js
+++ app/scripts/services/tracking-service.js
@@ -4,12 +4,10 @@ angular.module('lmisChromeApp').service('trackingService', function ($window) {
    var _tracker; 
     angular.element(document).ready(function () {
         var _service = $window.analytics.getService('LoMIS');
-//  //put your own code here!
-//   _tracker = _service.getTracker('UA-51340003-1');
-    });
+      //put your own code here!
+     _tracker = _service.getTracker('UA-51340003-1');
+   });
 
-
-  
 
   this.getTracker =  function(){
     return _tracker;
