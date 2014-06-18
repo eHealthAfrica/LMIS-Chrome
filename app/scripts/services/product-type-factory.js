b5b2df864d47f03ac2b264c29472676459ad5918
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-06-18 12:51:37 +0100

    add getBatch to product type factory. functions refs item:643

--- app/scripts/services/product-type-factory.js
+++ app/scripts/services/product-type-factory.js
@@ -1,65 +1,81 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-    .factory('productTypeFactory', function ($q, storageService, uomFactory) {
+  .factory('productTypeFactory', function ($q, storageService, uomFactory) {
 
-      var getByUuid = function(uuid) {
-        var deferred = $q.defer();
-        storageService.find(storageService.PRODUCT_TYPES, uuid)
-            .then(function (productType) {
-              if (productType !== undefined) {
-                var promises = {
-                  base_uom: uomFactory.get(productType.base_uom)
-                };
+    var getByUuid = function (uuid) {
+      var deferred = $q.defer();
+      storageService.find(storageService.PRODUCT_TYPES, uuid)
+        .then(function (productType) {
+          if (productType !== undefined) {
+            var promises = {
+              base_uom: uomFactory.get(productType.base_uom)
+            };
 
-                $q.all(promises)
-                    .then(function(result){
-                      for(var key in result) {
-                        productType[key] = result[key];
-                      }
-                      deferred.resolve(productType);
-                    })
-                    .catch(function(reason){
-                      deferred.reject(reason);
-                    });
-              } else {
+            $q.all(promises)
+              .then(function (result) {
+                for (var key in result) {
+                  productType[key] = result[key];
+                }
                 deferred.resolve(productType);
-              }
+              })
+              .catch(function (reason) {
+                deferred.reject(reason);
+              });
+          } else {
+            deferred.resolve(productType);
+          }
+        })
+        .catch(function (reason) {
+          deferred.reject(reason);
+        });
+      return deferred.promise;
+    };
+
+    var getProductTypeList = function () {
+      var deferred = $q.defer();
+
+      storageService.all(storageService.PRODUCT_TYPES)
+        .then(function (data) {
+          var promises = [];
+          for (var index in data) {
+            var productType = data[index];
+            promises.push(getByUuid(productType.uuid));
+          }
+
+          $q.all(promises)
+            .then(function (results) {
+              deferred.resolve(results);
             })
             .catch(function (reason) {
               deferred.reject(reason);
             });
-        return deferred.promise;
-      };
+        })
+        .catch(function (reason) {
+          deferred.reject(reason);
+        });
+      return deferred.promise;
+    };
 
-      var getProductTypeList = function(){
-        var deferred = $q.defer();
+    var getProductTypesByBatch = function (lowStockUuidList) {
+      var deferred = $q.defer();
+      storageService.all(storageService.PRODUCT_TYPES)
+        .then(function(result){
+          var productTypesBatch = result.filter(function(elem){
+            return lowStockUuidList.indexOf(elem.uuid) !== -1;
+          });
+          deferred.resolve(productTypesBatch);
+        })
+        .catch(function(err){
+          deferred.reject(err);
+        });
+      return deferred.promise;
+    };
 
-        storageService.all(storageService.PRODUCT_TYPES)
-            .then(function (data) {
-              var promises = [];
-              for(var index in data){
-                var productType = data[index];
-                promises.push(getByUuid(productType.uuid));
-              }
+    return {
+      getBatch: getProductTypesByBatch,
+      getAll: getProductTypeList,
+      get: getByUuid
+    };
 
-              $q.all(promises)
-                  .then(function(results){
-                    deferred.resolve(results);
-                  })
-                  .catch(function(reason){
-                    deferred.reject(reason);
-                  });
-            })
-            .catch(function(reason){
-              deferred.reject(reason);
-            });
-        return deferred.promise;
-      };
-
-      return {
-        getAll: getProductTypeList,
-        get: getByUuid
-      };
-
-    });
+  });
