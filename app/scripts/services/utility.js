beed71796c8a23562a735e5a5414c02b0d44b6de
Tom Vincent <git@tlvince.com>
2014-06-19 12:53:15 +0100

    Delint utility service, refs item:557

--- app/scripts/services/utility.js
+++ app/scripts/services/utility.js
@@ -1,14 +1,18 @@
 'use strict';
 
-angular.module('lmisChromeApp').service('utility', function ($location, $anchorScroll, $filter) {
+angular.module('lmisChromeApp')
+.service('utility', function ($location, $anchorScroll, $filter) {
 
   /**
    * This spaces out string concatenated by -
-   * @param name - string to be re-formatted
+   * @param {string} name - string to be re-formatted
    * @returns {XML|string}
    */
-  this.getReadableProfileName = function(name){
-    return name.replace(/\-/g,' - ').replace(/([0-9])([a-zA-Z])/g,'$1 $2').replace(/([a-z][a-z])([A-Z])/g,'$1 $2');
+  this.getReadableProfileName = function(name) {
+    return name
+      .replace(/\-/g, ' - ')
+      .replace(/([0-9])([a-zA-Z])/g, '$1 $2')
+      .replace(/([a-z][a-z])([A-Z])/g, '$1 $2');
   };
 
   /**
@@ -18,7 +22,11 @@ angular.module('lmisChromeApp').service('utility', function ($location, $anchorS
     //TODO: this needs to be a global function with better timezone calculation
     //TODO: ref https://bitbucket.org/pellepim/jstimezonedetect
     var tz = new Date().getTimezoneOffset() / 60;
-    return (tz < 0) ? parseInt('+' + Math.abs(tz)) : parseInt('-' + Math.abs(tz));
+    if (tz < 0) {
+      return parseInt('+' + Math.abs(tz), 10);
+    } else {
+      return parseInt('-' + Math.abs(tz), 10);
+    }
   };
 
   /**
@@ -28,33 +36,44 @@ angular.module('lmisChromeApp').service('utility', function ($location, $anchorS
    * @returns {{}}
    */
 
-  this.castArrayToObject = function(array, id){
+  this.castArrayToObject = function(array, id) {
     id = angular.isUndefined(id) ? 'uuid' : id;
     var newObject = {};
-    if(Object.prototype.toString.call(array) === '[object Array]'){
-      for(var i=0; i < array.length; i++){
+    if (Object.prototype.toString.call(array) === '[object Array]') {
+      for (var i=0; i < array.length; i++) {
         newObject[array[i][id]] = array[i];
       }
     }
     return newObject;
   };
 
-  this.getWeekRangeByDate = function(date, reminderDay){
+  this.getWeekRangeByDate = function(date, reminderDay) {
     var currentDate = date;
-    // First day of current week is assumed to be Sunday, if current date is 19-12-2014, which is Thursday = 4,
-    //then date of first day of current week = 19 - 4 = 15-12-2014 which is Sunday
+    // First day of current week is assumed to be Sunday, if current date is
+    // 19-12-2014, which is Thursday = 4, then date of first day of current week
+    // = 19 - 4 = 15-12-2014 which is Sunday
     var firstDayOfCurrentWeek = currentDate.getDate() - currentDate.getDay();
     var FIRST_DAY_AND_LAST_DAY_DIFF = 6;
-    var lastDayOfCurrentWeek = firstDayOfCurrentWeek + FIRST_DAY_AND_LAST_DAY_DIFF;
+    var lastDayOfCurrentWeek = firstDayOfCurrentWeek +
+      FIRST_DAY_AND_LAST_DAY_DIFF;
 
-    var firstDayDateOfCurrentWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(),
-        firstDayOfCurrentWeek, 0, 0, 0);
+    var firstDayDateOfCurrentWeek = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      firstDayOfCurrentWeek, 0, 0, 0
+    );
 
-    var lastDayDateOfCurrentWeek = new Date(currentDate.getFullYear(), currentDate.getMonth(),
-        lastDayOfCurrentWeek, 0, 0, 0);
+    var lastDayDateOfCurrentWeek = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      lastDayOfCurrentWeek, 0, 0, 0
+    );
 
-    var reminderDate = new Date(currentDate.getFullYear(), currentDate.getMonth(),
-        firstDayOfCurrentWeek + reminderDay, 0, 0, 0);
+    var reminderDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      firstDayOfCurrentWeek + reminderDay, 0, 0, 0
+    );
 
     return {
       'first': firstDayDateOfCurrentWeek,
@@ -69,34 +88,34 @@ angular.module('lmisChromeApp').service('utility', function ($location, $anchorS
    *
    * #see 'top' is the id of a href element defined in views/index/index.html
    */
-  this.scrollToTop = function(){
+  this.scrollToTop = function() {
     $location.hash('top');
     $anchorScroll();
   };
 
-  var isDateObject = function(date){
+  var isDateObject = function(date) {
     return Object.prototype.toString.call(date) === '[object Date]';
   };
 
-  this.getFullDate = function(date){
+  this.getFullDate = function(date) {
     //TODO: add validation for invalid date object.
-    if( !isDateObject(date)){
+    if (!isDateObject(date)) {
       date = new Date(date);//create date object
     }
     return $filter('date')(date, 'yyyy-MM-dd');
   };
 
-  var removeObjFromCollection = function(obj, collection, key){
+  var removeObjFromCollection = function(obj, collection, key) {
     collection = collection.filter(function (item) {
-      if(typeof item[key] === 'undefined' || typeof obj[key] === 'undefined'){
-        throw 'both objects that are being compared must have the property(key) being compared.';
+      if (typeof item[key] === 'undefined' || typeof obj[key] === 'undefined') {
+        throw 'both objects compared must have the property(key).';
       }
       return item[key] !== obj[key];
     });
     return collection;
   };
 
-  this.addObjectToCollection = function(obj, collections, key){
+  this.addObjectToCollection = function(obj, collections, key) {
     var _obj = JSON.parse(obj);
     if (_obj.deSelected === undefined) {
       collections.push(_obj);
@@ -105,11 +124,11 @@ angular.module('lmisChromeApp').service('utility', function ($location, $anchorS
     return removeObjFromCollection(_obj, collections, key);
   };
 
-  this.spaceOutUpperCaseWords = function(upperCaseWord){
+  this.spaceOutUpperCaseWords = function(upperCaseWord) {
     return upperCaseWord.split(/(?=[A-Z])/).join(' ');
   };
 
-  this.copy = function(src, des){
+  this.copy = function(src, des) {
     if (typeof src !== 'undefined') {
       //record already exists, update its fields that exist on data
       var properties = Object.keys(des);
@@ -123,10 +142,10 @@ angular.module('lmisChromeApp').service('utility', function ($location, $anchorS
   };
 
   this.ellipsize = function(string, length) {
-    if(length < 1) {
+    if (length < 1) {
       return '';
     }
-    if(string && string.length > length) {
+    if (string && string.length > length) {
       string = string.substr(0, length - 1) + '…';
     }
     return string;
