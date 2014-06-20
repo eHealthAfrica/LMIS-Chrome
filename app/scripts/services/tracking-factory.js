1320ab1b2439868716bdaac8fc242d88c1dc5bb7
thanasio <a.anthopoulos@ucl.ac.uk>
2014-06-20 21:39:07 +0100

    Merge remote-tracking branch 'upstream/develop' into HEAD
    Conflicts:
    	.gitignore
    	app/index.html
    	app/manifest.json
    	app/scripts/services/uomfactory.js
    	app/views/app-config/edit-configuration.html
    	app/views/app-config/partials/forms/ccu-profile.html
    	app/views/app-config/partials/forms/product-profile.html
    	app/views/app-config/welcome-page.html
    	app/views/app-config/wizard/partials/email-setup.html
    	app/views/app-config/wizard/partials/facility-cce-profile-setup.html
    	app/views/app-config/wizard/partials/facility-contact-person.html
    	app/views/app-config/wizard/partials/facility-info.html
    	app/views/app-config/wizard/partials/facility-product-profile.html
    	app/views/app-config/wizard/partials/facility-survey.html
    	app/views/batches/add-batch-form.html
    	app/views/batches/index.html
    	app/views/bundles/partials/bundle-lines.html
    	app/views/bundles/partials/enter-bundle-no.html
    	app/views/bundles/partials/log-automatically.html
    	app/views/bundles/partials/log-incoming-preview.html
    	app/views/cold-chain-units/report-ccu-breakdown.html
    	app/views/dashboard/dashboard.html
    	app/views/dashboard/partials/stock-out-warning.html
    	app/views/facilities/index.html
    	app/views/home/dashboard.html
    	app/views/home/main-activity.html
    	app/views/home/partials/discard-count-reminder.html
    	app/views/home/partials/stock-count-reminder.html
    	app/views/home/partials/survey-reminder.html
    	app/views/home/settings.html
    	app/views/index/footer.html
    	app/views/index/header.html
    	app/views/inventory/index.html
    	app/views/notification-service/confirm-dialog.html
    	app/views/orders/place-order.html
    	app/views/partials/group_about_to_expire_in_months.html
    	app/views/partials/notifications.html
    	app/views/partials/search.html
    	app/views/product-types/add-product-type.html
    	app/views/product-types/product-types-list.html
    	app/views/programs/product_profile.html
    	app/views/programs/product_profile_form.html
    	app/views/programs/program_form.html
    	app/views/programs/program_product_form.html
    	app/views/reminder/reminder-notification.html
    	app/views/stock-count/index.html
    	app/views/stock-count/stock-count-form.html
    	app/views/stock-out-broadcast/stock-out-broadcast.html
    	app/views/survey/take-survey.html
    	app/views/waste-count/waste-count-form2.html
    	app/views/waste-count/waste-count.html

--- app/scripts/services/tracking-factory.js
+++ app/scripts/services/tracking-factory.js
@@ -3,25 +3,45 @@
 //ok so here we need to overload sendAppView, sendException and sendEvent to write on local storage (JSON)
 //use local storage or couchdb service to store hits temporarilly
 angular.module('lmisChromeApp')
-        .factory('trackingFactory', function($window, $rootScope, config, deviceInfoFactory) {
+        .factory('trackingFactory', function($window, $rootScope, config, deviceInfoFactory, storageService) {
 
             var tracker;
             if ('analytics' in $window) {
                 var service = $window.analytics.getService(config.analytics.service);
 
                 tracker = (deviceInfoFactory.isOnline()) ? service.getTracker(config.analytics.propertyID) : {
+                   
                     sendAppView: function(page) {
-                        console.log("offline page: "+page);
+                        console.log("offline page: " + page);
+                        var _pageview ={
+                            page: page
+                        };
+//                        storageService.loadTableObject(storageService.PAGE_VIEWS);
+                        storageService.save(storageService.PAGE_VIEWS, _pageview);
                     },
                     sendException: function(opt_description, opt_fatal) {
-                        console.log("offline exception : "+opt_description +": " + opt_fatal);
+                        console.log("offline exception : " + opt_description + ": " + opt_fatal);
+                        var _exception = {
+                            opt_description: opt_description,
+                            opt_fatal: opt_fatal
+                        };
+                        storageService.save(storageService.EXCEPTIONS, _exception);
                     },
-                    sendEvent: function(category, action, label){
-                        console.log("offline click : "+category +": " + action + ": " + label);
+                    sendEvent: function(category, action, label) {
+                        console.log("offline click : " + category + ": " + action + ": " + label);
+                        var _event = {
+                            category: category,
+                            action: action,
+                            label: label
+                        };
+                        var clcks = storageService.CLICKS;
+                        console.log("clicks: " + clcks);
+                        storageService.save(storageService.CLICKS, _event);
+
                     }
 
                 }
-                $rootScope.$on('$stateChangeSuccess', function( state) {
+                $rootScope.$on('$stateChangeSuccess', function(state) {
                     tracker.sendAppView(state.name);
                 });
 
@@ -29,9 +49,9 @@ angular.module('lmisChromeApp')
                     tracker.sendException(state.to, false);
                 });
             }
-            
 
-            
+
+
 
             return {
                 tracker: tracker
