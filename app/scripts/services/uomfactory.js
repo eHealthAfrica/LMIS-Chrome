9c947df9f35dfb641fd7c43edc7e8d0f668493ea
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-06-23 11:08:02 +0100

    uses utility.getStringUuid to uom-factory

--- app/scripts/services/uomfactory.js
+++ app/scripts/services/uomfactory.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-    .factory('uomFactory', function ($q, storageService, trackingFactory) {
+    .factory('uomFactory', function ($q, storageService, trackingFactory, utility) {
 
       var getByUuid = function(uuid) {
         var deferred = $q.defer();
         //TODO: attach nested objects
+        var uuid = utility.getStringUuid(uuid);
         storageService.find(storageService.UOM, uuid)
             .then(function (data) {
               deferred.resolve(data);
@@ -22,7 +23,8 @@ angular.module('lmisChromeApp')
         storageService.all(storageService.UOM).then(function (data) {
           for(var index in data){
             var uom = data[index];
-            uomList.push(getByUuid(uom.uuid));
+            var uuid =  utility.getStringUuid(uom);
+            uomList.push(getByUuid(uuid));
           }
 
           $q.all(uomList)
