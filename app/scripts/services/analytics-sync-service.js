605c544b53b5bc228370162682a691afdda1412f
thanasis <a.anthopoulos@ucl.ac.uk>
2014-06-26 06:41:51 +0100

    Offline analytics commits: nearly there!
    Cheers,
    T

--- app/scripts/services/analytics-sync-service.js
+++ app/scripts/services/analytics-sync-service.js
@@ -5,60 +5,111 @@ angular.module('lmisChromeApp').service('analyticsSyncService', function($q, sto
 
     var tracker = trackingFactory.tracker;
     this.syncClicks = function() {
-        
+        var uuids = [];
+        console.log("Loopin")
         storageService.all(storageService.CLICKS).then(function(clicksData) {
             clicksData.forEach(function(click) {
+
                 console.log("uuid: " + click.uuid);
-//            console.log("timestamp: " + click.created);
-//            console.log("category: " + click.category);
-//            console.log("action: " + click.action);
-//            console.log("label: " + click.label);
-//            console.log("online: "+ trackingFactory.online);
+                console.log("online: " + trackingFactory.online);
                 if (trackingFactory.online) {
 
-                //best to get a success flag here and delete if event successfuly sent
-                tracker.sendEvent("Offline Clicks", click.action, click.label);
-                var deferred = $q.defer();
-                storageService.removeRecord(storageService.CLICKS, click.uuid)
-                            .then(function() {
-                                deferred.resolve();
-                            })
-                            .catch(function() {
-                                deferred.reject();
-                            });
+                    //best to get a success flag here and delete if event successfuly sent
+                    tracker.sendEvent("Offline Clicks", click.action, click.label);
+                    console.log(tracker.sendEvent("Offline Clicks", click.action, click.label));
+                    uuids.push(click.uuid);
+
                 }
             });
         }).finally(function() {
             console.log("pending list cleared (i wish!)");
         });
+
+        console.log("uuids: " + uuids.length);
+        var deferred = $q.defer();
+        storageService.removeRecords(storageService.CLICKS, uuids)
+                .then(function() {
+                    deferred.resolve();
+                })
+                .catch(function() {
+                    deferred.reject();
+                });
     };
 
     //not final yet. Work in progress
     this.syncExceptions = function() {
 
+        var uuids = [];
+        console.log("Loopin Excepts")
+
         storageService.all(storageService.EXCEPTIONS).then(function(exceptionData) {
             exceptionData.forEach(function(exception) {
+
                 console.log("uuid: " + exception.uuid);
-                console.log("timestamp: " + exception.created);
-                console.log("opt_dexcription: " + exception.opt_description);
-                console.log("action: " + exception.opt_fatal);
-                tracker.sendException(exception.opt_description, exception.opt_fatal);
-                storageService.removeRecord(storageService.EXCEPTIONS, exception.uuid);
+                console.log("online: " + trackingFactory.online);
+                if (trackingFactory.online) {
+
+                    //best to get a success flag here and delete if event successfuly sent
+                    tracker.sendEvent(exception.opt_description, exception.opt_fatal);
+                    console.log(tracker.sendEvent("Offline excepts", exception.opt_description, exception.opt_fatal));
+                    uuids.push(exception.uuid);
+
+                }
             });
+
+            console.log("uuids: " + uuids.length);
+            var deferred = $q.defer();
+            storageService.removeRecords(storageService.EXCEPTIONS, uuids)
+                    .then(function() {
+                        deferred.resolve();
+                    })
+                    .catch(function() {
+                        deferred.reject();
+                    });
+
+
+        }).finally(function() {
+            console.log("pending list cleared (i wish!)");
         });
+
     };
 
     //not final yet. Worki in progress
     this.syncPageViews = function() {
 
+        
+        var uuids = [];
+        console.log("Loopin Excepts")
+
         storageService.all(storageService.PAGE_VIEWS).then(function(pageViewData) {
             pageViewData.forEach(function(pageView) {
+
                 console.log("uuid: " + pageView.uuid);
-                console.log("timestamp: " + pageView.created);
-                console.log("opt_dexcription: " + pageView.page);
-                tracker.sendAppView(pageView.page);
-                storageService.removeRecord(storageService.PAGE_VIEWS, pageView.uuid);
+                console.log("online: " + trackingFactory.online);
+                if (trackingFactory.online) {
+
+                    //best to get a success flag here and delete if event successfuly sent
+                    tracker.sendAppView(pageView.page);
+                    console.log(tracker.sendEvent("Offline views", pageView.page));
+                    uuids.push(pageView.uuid);
+
+                }
             });
+
+            console.log("uuids: " + uuids.length);
+            var deferred = $q.defer();
+            storageService.removeRecords(storageService.EXCEPTIONS, uuids)
+                    .then(function() {
+                        deferred.resolve();
+                    })
+                    .catch(function() {
+                        deferred.reject();
+                    });
+
+
+        }).finally(function() {
+            console.log("pending list cleared (i wish!)");
         });
+        
     };
 });
