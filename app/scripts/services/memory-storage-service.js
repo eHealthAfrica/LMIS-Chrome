ad121613c7ce6f8d58096e9c9eae79cd5a01a6b6
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-06-26 09:54:42 +0100

    created memory storage service for in-memory models.

--- app/scripts/services/memory-storage-service.js
+++ app/scripts/services/memory-storage-service.js
@@ -1,7 +1,8 @@
 angular.module('lmisChromeApp')
-  .service('memoryStorageService', function () {
+  .service('memoryStorageService', function ($rootScope, cacheService) {
 
-    var memoryStore = {};
+    $rootScope.memoryStore = {};
+    var MEMORY_STORE = 'memory_store';
 
     this.put = function(dbName, data){
       if(!('uuid' in data)){
@@ -10,23 +11,35 @@ angular.module('lmisChromeApp')
       if(typeof dbName !== 'string' || dbName.length === 0){
         throw ['dbName is not a string or is an empty string. db name: ', JSON.stringify(dbName)].join(' ');
       }
-      if(typeof memoryStore[dbName] === 'undefined'){
-        memoryStore[dbName] = {};
+      if(typeof $rootScope.memoryStore[dbName] === 'undefined'){
+        $rootScope.memoryStore[dbName] = {};
       }
-      memoryStore[dbName][data.uuid] = data;
+      $rootScope.memoryStore[dbName][data.uuid] = data;
+      cacheService.put(MEMORY_STORE, $rootScope.memoryStore);
     };
 
     this.get = function(dbName, key){
-      var db = memoryStore[dbName];
+      if(typeof $rootScope.memoryStore !== 'object'){
+        console.error('memory store is not available.');
+      }
+      if(Object.keys($rootScope.memoryStore).length === 0){
+        console.error('memory store is empty.');
+      }
+      dbName = String(dbName);
+      key = String(key);
+      var db = $rootScope.memoryStore[String(dbName)];
       if(typeof db !== 'object'){
         console.error('dbName: '+dbName+', does not exist, key: '+key+', db: '+db);
+        $rootScope.memoryStore = cacheService.get(MEMORY_STORE);//reload memory store from cache.
+        db = $rootScope.memoryStore[String(dbName)];
       }
-      var record = db[String(key)];
+      var record = db[key];
       return record;
     };
 
     this.delete = function(dbName, key){
-      delete memoryStore[dbName][key];
+      delete $rootScope.memoryStore[dbName][key];
+      cacheService.put(MEMORY_STORE, $rootScope.memoryStore);
     };
 
     /**
@@ -38,20 +51,28 @@ angular.module('lmisChromeApp')
      * @param db
      */
     this.setDatabase = function(dbName, db){
-      //TODO: check if db is set already, if set throw an exception that you might over-write the db.
-      memoryStore[dbName] = db;
+      //TODO:(discuss) check if db is set already, if set throw an exception that you might over-write the db.
+      $rootScope.memoryStore[dbName] = db;
+      cacheService.put(MEMORY_STORE, $rootScope.memoryStore);
     };
 
     this.getDatabase = function(dbName){
-      return memoryStore[dbName];
+      var db = $rootScope.memoryStore[dbName];
+      if(typeof db !== 'object'){
+        var mem = cacheService.get(MEMORY_STORE);
+        db = mem[dbName];
+      }
+      return db;
     };
 
     this.deleteDatabase = function(dbName){
-      delete memoryStore[dbName];
+      delete $rootScope.memoryStore[dbName];
+      cacheService.put(MEMORY_STORE, $rootScope.memoryStore);
     };
 
     this.clearAll = function(){
-      memoryStore = {};
+      $rootScope.memoryStore = {};
+      cacheService.put(MEMORY_STORE, $rootScope.memoryStore);
     };
 
   });
