1de763b54e022d15115f585fd30fbdd0d3927da3
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-06-26 09:45:04 +0100

    corrected and added more tests

--- test/spec/services/product-category-factory-spec.js
+++ test/spec/services/product-category-factory-spec.js
@@ -3,32 +3,65 @@
 describe('Factory: productCategoryFactory', function () {
 
   // load the service's module
-  beforeEach(module('lmisChromeApp', 'i18nMocks'));
+  beforeEach(module('lmisChromeApp', 'i18nMocks', 'memoryStoreMocks'));
 
-  // instantiate factory
-  var productCategoryFactory, storageService;
+  // instantiate dependencies
+  var productCategoryFactory;
+  var storageService;
+  var memoryStorageService;
+  var memoryStore;
 
-  beforeEach(inject(function (_productCategoryFactory_, _storageService_) {
+  beforeEach(inject(function (_productCategoryFactory_, _storageService_, _memoryStorageService_, memoryStoreMock, cacheService) {
     productCategoryFactory = _productCategoryFactory_;
     storageService = _storageService_;
-    spyOn(storageService, 'get').andCallThrough();
+    memoryStorageService = _memoryStorageService_;
+    memoryStore = memoryStoreMock.memoryStore;
+
+    spyOn(memoryStorageService, 'get').andCallFake(function(dbName, key){
+      return memoryStoreMock.memoryStore[dbName][key];
+    });
+
+    spyOn(cacheService, 'get').andCallFake(function () {
+      return memoryStoreMock.memoryStore
+    });
+
+    spyOn(memoryStorageService, 'getDatabase').andCallFake(function (dbName) {
+      return memoryStore[dbName];
+    });
+
   }));
 
   it('i expect productCategoryFactory to be defined ', function () {
     expect(productCategoryFactory).toBeDefined();
   });
 
-  it('i expect productCategoryFactory.get() to call storageService.get with the right parameters.', function () {
-    expect(storageService.get).not.toHaveBeenCalled();
-    var uuid = '1234-abe37-2182h';
-    productCategoryFactory.get(uuid);
-    expect(storageService.get).toHaveBeenCalledWith(storageService.PRODUCT_CATEGORY);
+  it('i expect productCategoryFactory.get() to return expected product category.', function () {
+    var uuid = '1c761db0-d7f3-4abf-8c12-6c678f862851';//dry store safety boxes
+    var result = productCategoryFactory.get(uuid);
+    var expectedResult = memoryStore[storageService.PRODUCT_CATEGORY][uuid];
+    expect(result).toBe(expectedResult);
+    expect(result.name).toBe('Dry Store Safety Boxes');
   });
 
-  it('i expect productCategoryFactory.getAll() to call storageService.get with the right parameters.', function () {
-    expect(storageService.get).not.toHaveBeenCalled();
+  it('i expect get() to call memoryStorageService.getDatabase() with the right parameter.', function(){
+    expect(memoryStorageService.getDatabase).not.toHaveBeenCalled();
+    var uuid = '1c761db0-d7f3-4abf-8c12-6c678f862851';//dry store safety boxes
+    productCategoryFactory.get(uuid);
+    expect(memoryStorageService.getDatabase).toHaveBeenCalledWith(storageService.PRODUCT_CATEGORY);
+  });
+
+  it('i expect productCategoryFactory.getAll() to call memoryStorageService.getDatabase() with the right parameters.', function () {
+    expect(memoryStorageService.getDatabase).not.toHaveBeenCalled();
     productCategoryFactory.getAll();
-    expect(storageService.get).toHaveBeenCalledWith(storageService.PRODUCT_CATEGORY);
+    expect(memoryStorageService.getDatabase).toHaveBeenCalledWith(storageService.PRODUCT_CATEGORY);
+  });
+
+  it('i expect productCategoryFactory.getAll() to return an array of product categories.', function () {
+    expect(memoryStorageService.getDatabase).not.toHaveBeenCalled();
+    var result = productCategoryFactory.getAll();
+    expect(result.length).toBeGreaterThan(0);
+    var elem = result[0];
+    expect(elem).toBeDefined();
   });
 
 });
--- test/spec/services/product-profile-factory-spec.js
+++ test/spec/services/product-profile-factory-spec.js
@@ -3,18 +3,38 @@
 describe('Factory: productProfileFactory', function () {
 
   // load the service's module
-  beforeEach(module('lmisChromeApp', 'i18nMocks'));
+  beforeEach(module('lmisChromeApp', 'i18nMocks', 'memoryStoreMocks'));
 
-  // instantiate factory
-  var productProfileFactory, storageService, productCategoryFactory, $q;
+  // instantiate dependencies
+  var productProfileFactory;
+  var storageService;
+  var productCategoryFactory;
+  var $q;
+  var memoryStorageService;
+  var memoryStore;
 
-  beforeEach(inject(function (_productProfileFactory_, _storageService_, _productCategoryFactory_, _$q_) {
+  beforeEach(inject(function (_productProfileFactory_, cacheService, _storageService_, _productCategoryFactory_, _$q_, _memoryStorageService_, memoryStoreMock) {
     productProfileFactory = _productProfileFactory_;
     storageService = _storageService_;
     productCategoryFactory = _productCategoryFactory_;
     $q = _$q_;
+    memoryStorageService = _memoryStorageService_;
+    memoryStore = memoryStoreMock.memoryStore;
 
-    spyOn(storageService, 'find').andCallThrough();
+    spyOn(cacheService, 'get').andCallFake(function () {
+      return memoryStoreMock.memoryStore
+    });
+
+    spyOn(memoryStorageService, 'get').andCallFake(function (dbName, key) {
+      var db = memoryStore[dbName];
+      var record = db[key];
+      return record;
+    });
+
+    spyOn(memoryStorageService, 'getDatabase').andCallFake(function (dbName) {
+      return memoryStore[dbName];
+    });
+
     spyOn(storageService, 'all').andCallThrough();
     spyOn(productCategoryFactory, 'getKeyValuePairs').andCallThrough();
     spyOn($q, 'all').andCallThrough();
@@ -25,30 +45,134 @@ describe('Factory: productProfileFactory', function () {
     expect(productProfileFactory).toBeDefined();
   });
 
-  it('i expect productProfileFactory.get() to call storageService.find with the right parameters.', function () {
-    expect(storageService.find).not.toHaveBeenCalled();
-    var uuid = '1234-abe37-2182h';
+  it('i expect productProfileFactory.get() to call memoryStorageService.get() with the right parameters.', function () {
+    expect(memoryStorageService.get).not.toHaveBeenCalled();
+    var uuid = '075bd789-4b29-4033-80b6-4f834e602628';//BCG 20
     productProfileFactory.get(uuid);
-    expect(storageService.find).toHaveBeenCalledWith(storageService.PRODUCT_PROFILE, uuid);
+    expect(memoryStorageService.get).toHaveBeenCalledWith(storageService.PRODUCT_PROFILE, uuid);
   });
 
-  it('i expect productProfileFactory.getAll() to call storageService.all with the right parameter.', function () {
-    expect(storageService.all).not.toHaveBeenCalled();
+  it('i expect productProfileFactory.get() to return product profile if record exist.', function () {
+    var uuid = '075bd789-4b29-4033-80b6-4f834e602628';//BCG 20
+    var result = productProfileFactory.get(uuid);
+    var expectedResult = memoryStore[storageService.PRODUCT_PROFILE][uuid];
+    expect(result).toBeDefined();
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('i expect productProfileFactory.getAll() to call memoryStorageService.getDatabase() with the right parameter.', function () {
+    expect(memoryStorageService.getDatabase).not.toHaveBeenCalled();
     productProfileFactory.getAll();
-    expect(storageService.all).toHaveBeenCalledWith(storageService.PRODUCT_PROFILE);
+    expect(memoryStorageService.getDatabase).toHaveBeenCalledWith(storageService.PRODUCT_PROFILE);
   });
 
-  it('i expect getAllGroupedByCategory() to call $q.all(), storageServe.all and prod-category-factory.getKeyValuePairs',
-    function(){
-      expect(storageService.all).not.toHaveBeenCalled();
-      expect($q.all).not.toHaveBeenCalled();
-      expect(productCategoryFactory.getKeyValuePairs).not.toHaveBeenCalled();
-      productProfileFactory.getAllGroupedByCategory();
+  it('i expect productProfileFactory.getAll() to return an array of product profile.', function () {
+    var result = productProfileFactory.getAll();
+    expect(angular.isArray(result)).toBeTruthy();
+    expect(result.length).toBeGreaterThan(0)
+  });
 
-      expect(storageService.all).toHaveBeenCalledWith(storageService.PRODUCT_PROFILE);
-      expect(productCategoryFactory.getKeyValuePairs).toHaveBeenCalled();
-      expect($q.all).toHaveBeenCalled();
+  it('i expect getAllGroupedByCategory() to return list of prod. profiles grouped by their category', function () {
+    var result = productProfileFactory.getAllGroupedByCategory();
+    var groupKeys = Object.keys(result);
+    expect(groupKeys.length).toBeGreaterThan(2);//at least there is two category dry goods and cold store
+    var coldStoreVaccineGrp = result['66e1bc4f-1dab-4842-80c5-49f626bde74e'];//cold store vaccine group
+    expect(coldStoreVaccineGrp.category.name).toEqual('Cold Store Vaccines');
+    expect(coldStoreVaccineGrp.productProfiles.length).toBeGreaterThan(0);
+  });
+
+  it('i expect getBatchGroupedByCategory(batch) to return only categories of product profiles in the given batch.', function () {
+    var batch = [
+      {
+        "uuid": "075bd789-4b29-4033-80b6-4f834e602628",
+        "created": "2014-01-30T11:13:53.058Z",
+        "modified": "2014-01-30T11:13:53.144Z",
+        "is_deleted": false,
+        "created_by": 1,
+        "modified_by": 1,
+        "name": "BCG 20",
+        "long_name": "BCG-20-DPV-ID-1.2Vol-Freeze-Dried-WHO-CSV",
+        "product": "e55e1452-b0ab-4046-9d7e-3a98f1f968d0",
+        "category": "66e1bc4f-1dab-4842-80c5-49f626bde74e",
+        "presentation": "299939e1-94d7-4e8e-bfb7-c008af1692af",
+        "formulation": "d2a1ae75-8fb0-4c80-80f7-0b05ba445b8a",
+        "mode_of_use": "d4957c68-d977-4ff2-a5c2-3e8538d302cd",
+        "description": "",
+        "packed_volume": 1.2,
+        "diluent_per_volume": 0.7,
+        "volume_uom": "6e2a882b-eeeb-4caf-96e2-46167c0c1aac"
+      },
+      'f97be2aa-d5b6-4560-8f31-5a559fb80567',
+      '14f512076fe8-73087d12-fbf0-4024-ea3a',
+    ]; //BCG 10, Penta, ADS syringe //2 cold store vaccines and  1 dry store
+
+    var result = productProfileFactory.getBatchGroupedByCategory(batch);
+    var resultCategories = Object.keys(result);
+    expect(resultCategories.length).toBe(2);//cold store va and dry goods.
+
+    var coldStoreVaccineGrp = result['66e1bc4f-1dab-4842-80c5-49f626bde74e'];//cold store vaccine group
+    expect(coldStoreVaccineGrp.productProfiles.length).toBe(2);
+
+    var dryGoodsGrp = result['d5ffa6bd-58d2-41b5-a688-6a9d6894c5ae'];
+    expect(dryGoodsGrp.productProfiles.length).toBe(1);
+  });
+
+  it('i expect productProfileFactory.getBatch(batch) to return array of product profile of given batch, obj/uuid.', function () {
+    var batch = [
+      "d1a1d9958b1c-4f698a8d-5477-4f55-9811",
+      {
+        "uuid": "075bd789-4b29-4033-80b6-4f834e602628",
+        "created": "2014-01-30T11:13:53.058Z",
+        "modified": "2014-01-30T11:13:53.144Z",
+        "is_deleted": false,
+        "created_by": 1,
+        "modified_by": 1,
+        "name": "BCG 20",
+        "long_name": "BCG-20-DPV-ID-1.2Vol-Freeze-Dried-WHO-CSV",
+        "product": "e55e1452-b0ab-4046-9d7e-3a98f1f968d0",
+        "category": "66e1bc4f-1dab-4842-80c5-49f626bde74e",
+        "presentation": "299939e1-94d7-4e8e-bfb7-c008af1692af",
+        "formulation": "d2a1ae75-8fb0-4c80-80f7-0b05ba445b8a",
+        "mode_of_use": "d4957c68-d977-4ff2-a5c2-3e8538d302cd",
+        "description": "",
+        "packed_volume": 1.2,
+        "diluent_per_volume": 0.7,
+        "volume_uom": "6e2a882b-eeeb-4caf-96e2-46167c0c1aac"
+      }
+    ];
+
+    var result = productProfileFactory.getBatch(batch);
+    expect(result.length).toBe(2);
+
+  });
+
+  it('i expect getByProductType(productType) to return array of product types of the given product type.', function () {
+    var productType = 'e55e1452-b0ab-4046-9d7e-3a98f1f968d0';//BCG
+    var result = productProfileFactory.getByProductType(productType);
+    expect(result.length).toBeGreaterThan(0);
+    for (var i in result) {
+      var pp = result[i];
+      expect(pp.product.code).toBe('BCG');
     }
-  );
+  });
+
+  it('i expect getByProductType(productType) to return EMPTY array if product type does not exist.', function () {
+    var productType = 'e55e1452-b0ab-4046-9d7e';//invalid product type
+    var result = productProfileFactory.getByProductType(productType);
+    expect(result.length).toBe(0);
+  });
+
+  it('i expect getByProductType(productType) to return product profile if called with object product type.', function(){
+    var productTypeObj = {
+      uuid: 'e55e1452-b0ab-4046-9d7e-3a98f1f968d0',
+      code: 'BCG'
+    }
+    var result = productProfileFactory.getByProductType(productTypeObj);
+    expect(result.length).toBeGreaterThan(0);
+    for (var i in result) {
+      var pp = result[i];
+      expect(pp.product.code).toBe('BCG');
+    }
+  });
 
 });
