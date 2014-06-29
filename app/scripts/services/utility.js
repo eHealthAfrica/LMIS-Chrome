ac23e7c0a85e23088cb416a2af6569f5377602bd
Tom Vincent <git@tlvince.com>
2014-06-29 15:07:00 +0100

    Tell JSHint uuidGenerator uses bit operators

--- app/scripts/services/utility.js
+++ app/scripts/services/utility.js
@@ -212,6 +212,7 @@ angular.module('lmisChromeApp')
     this.uuidGenerator = function() {
       var now = Date.now();
       return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        // jshint bitwise: false
         var r = (now + Math.random() * 16) % 16 | 0;
         now = Math.floor(now / 16);
         return (c === 'x' ? r : (r & 0x7 | 0x8)).toString(16);
