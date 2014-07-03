f1636d37c89018c1818766e3f6c098df990ab519
Tom Vincent <git@tlvince.com>
2014-07-03 23:00:05 +0100

    Null more rogue http requests, see item:680

--- test/spec/controllers/bundle.js
+++ test/spec/controllers/bundle.js
@@ -4,6 +4,28 @@ describe('Inventory controller', function () {
   // Load the controller's module
   beforeEach(module('lmisChromeApp', 'i18nMocks'));
 
+  beforeEach(inject(function ($templateCache) {
+    // Mock each template used by the state
+    var templates = [
+      'index/index',
+      'index/header',
+      'index/breadcrumbs',
+      'index/footer',
+      'home/index',
+      'home/nav',
+      'home/sidebar',
+      'home/control-panel',
+      'home/main-activity',
+      'home/home',
+      'dashboard/dashboard',
+      'index/loading-fixture-screen'
+    ];
+
+    angular.forEach(templates, function (template) {
+      $templateCache.put('views/' + template + '.html', '');
+    });
+  }));
+
   var $state;
   beforeEach(inject(function(_$state_) {
     $state = _$state_;
