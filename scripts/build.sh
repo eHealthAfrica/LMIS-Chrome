55141eea2a6f1632c100370c87f5bc86d2788854
Tom Vincent <git@tlvince.com>
2014-07-06 11:35:21 +0100

    Lock cca to v0.1.0, see item:724
    cca v0.1.1 is producing apks that are 10x as big as v0.1.0.

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,7 +15,7 @@ info "Performing $type build"
 [[ "$TRAVIS_TAG" ]] && grunt build:release || grunt build
 
 info "Building Mobile Chrome App"
-have "cca" || npm install -g cca
+have "cca" || npm install -g cca@0.1.0
 have "android" || error "Android SDK required"
 
 [[ -d "$build/$app" ]] && rm -rf "$build/$app"
