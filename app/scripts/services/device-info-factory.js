0f74d4aaa2093c249e44323fe15538a083b8f9c8
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-07-07 10:54:05 +0100

    adds canConnect() . refs item:693

--- app/scripts/services/device-info-factory.js
+++ app/scripts/services/device-info-factory.js
@@ -1,7 +1,51 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .factory('deviceInfoFactory', function($q, $window, utility) {
+  .factory('deviceInfoFactory', function($q, $window, utility, $interval, pouchStorageService) {
+    var THIRTY_SECS_DELAY = 30 * 1000;
+    var MAX_CONNECTION_ATTEMPT = 5;
+
+    /**
+     * This returns true if successful, else rejects with reason for failure.
+     *
+     * @returns {promise|Function|promise|promise|promise|*}
+     */
+    var canConnect = function() {
+      var deferred = $q.defer();
+      var testDb = 'connection_test';
+      var counter = 0;
+      var reason;
+      if (!$window.navigator.onLine) {
+        reason = 'device is not online, check your internet connection settings.';
+        deferred.reject(reason);
+      } else {
+        var syncRequest = $interval(function() {
+          if (counter < MAX_CONNECTION_ATTEMPT) {
+            try {
+              var remoteDb = pouchStorageService.getRemoteDB(testDb);
+              remoteDb.info()
+                .then(function() {
+                  $interval.cancel(syncRequest);//free $interval to avoid memory leak
+                  deferred.resolve(true);
+                  counter = MAX_CONNECTION_ATTEMPT;
+                })
+                .catch(function(conError) {
+                  reason = conError;
+                  counter = counter + 1;
+                });
+            } catch (e) {
+              reason = e;
+              counter = counter + 1;
+            }
+          } else {
+            deferred.reject(reason);//couldn't establish connection
+            $interval.cancel(syncRequest);//free $interval to avoid memory leak
+          }
+        }, THIRTY_SECS_DELAY);
+      }
+      return deferred.promise;
+    };
+
     return {
       getDeviceInfo: function() {
         var deferred = $q.defer();
@@ -35,6 +79,8 @@ angular.module('lmisChromeApp')
 
       isOnline: function(){
         return $window.navigator.onLine;
-      }
+      },
+
+      canConnect: canConnect
     };
   });
