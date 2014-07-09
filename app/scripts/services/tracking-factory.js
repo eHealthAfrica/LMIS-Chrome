37e8e92350cd2d4d0b0b20bec8470539f1a456e3
thanasis <a.anthopoulos@ucl.ac.uk>
2014-07-09 21:59:38 +0100

    offline analytics. Only thing left to do is to register the lost records

--- app/scripts/services/tracking-factory.js
+++ app/scripts/services/tracking-factory.js
@@ -47,6 +47,7 @@ angular.module('lmisChromeApp')
                             total += sizes[i];
                         }
                     }
+                    console.log("deleting: "+ toDelete.length)
                     storageService.removeRecords(table, toDelete);
                     deferred.resolve(toDelete.length);
 
@@ -57,19 +58,57 @@ angular.module('lmisChromeApp')
                             tracker.sendEvent(category, action, label);
                         })
                         .catch(function(reason) {
+                            console.log("offline click : " + category + ": " + action + ": " + label + " reason: " + reason);
                             var _event = {
                                 action: action,
                                 label: label
                             };
                             storageService.save(storageService.CLICKS, _event);
                             removeExcessRecords(storageService.CLICKS, events_limit).then(function(removed) {
-                })
-                return deferred.promise;
-            }
+//                                storageService.all(storageService.ANALYTICS_LOST_RECORDS).then(function(lostRecords) {
+//                                    console.log("removed: " + removed);
+//                                    lostRecords[0].events += removed;
+//                                    console.log("lrs: "+ lostRecords[0].events)
+//                                    storageService.removeRecord(storageService.ANALYTICS_LOST_RECORDS, lostRecords[0].uuid)
+//                                    storageService.insertData(storageService.ANALYTICS_LOST_RECORDS, lostRecords[0])
+//
+//                                });
+
+                            });
+                        }
+                        );
+            };
+
+
+            var appView = function(page) {
+                syncService.canConnect()
                         .then(function() {
                             tracker.sendAppView(page);
                         })
                         .catch(function(reason) {
+                            console.log("offline page: " + page);
+                            var _pageview = {
+                                page: page
+                            };
+                            storageService.save(storageService.PAGEVIEWS, _pageview);
+                            removeExcessRecords(storageService.PAGEVIEWS, pages_limit).then(function(removed) {
+                                storageService.get(storageService.ANALYTICS_LOST_RECORDS).then(function(lostRecords) {
+                                    console.log("removed: " + removed);
+                                    console.log("lost_recs: " + lostRecords);
+                                    console.log("lost_recs: " + lostRecords.length);
+                                    lostRecords[0].pages += removed;
+                                    storageService.removeRecord(storageService.ANALYTICS_LOST_RECORDS, lostRecords[0].uuid)
+                                    storageService.insertData(storageService.ANALYTICS_LOST_RECORDS, lostRecords[0])
+
+                                });
+
+                            });
+                        });
+
+            };
+
+            var exception = function(opt_description, opt_fatal) {
+
                 syncService.canConnect()
                         .then(function() {
                             tracker.sendException(opt_description, opt_fatal);
