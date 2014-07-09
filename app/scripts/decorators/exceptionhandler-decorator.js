0f6c125539ee12fa652796cf3d385829d04fd1a1
Tom Vincent <git@tlvince.com>
2014-07-09 16:46:09 +0100

    Report exceptions via decorator, closes item:740

---
+++ app/scripts/decorators/exceptionhandler-decorator.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('lmisChromeApp')
+  .config(function($provide) {
+    $provide.decorator('$exceptionHandler', function($delegate, $injector) {
+      var trackingFactory;
+      return function(exception, cause) {
+        $delegate(exception, cause);
+        trackingFactory = trackingFactory || $injector.get('trackingFactory');
+        trackingFactory.tracker.sendException(exception.message, false);
+      };
+    });
+  });
