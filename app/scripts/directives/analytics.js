1c0cd43b7923186b7a9419ed3d9a929d40ca38dc
Tom Vincent <git@tlvince.com>
2014-07-10 11:52:58 +0100

    Rewrite tracking factory as service, closes item:744

--- app/scripts/directives/analytics.js
+++ app/scripts/directives/analytics.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .directive('gaClick', function(trackingFactory) {
+  .directive('gaClick', function(trackingService) {
     return {
       restrict: 'A',
       link: function(scope, element, attr) {
-//        var tracker = tracker;
+        var tracker = trackingService.tracker;
         element.on('click', function() {
             console.log('Click: '+ element.text().trim()+": "+ attr.gaClick.trim());
           trackingFactory.postEvent('Click', element.text().trim(), attr.gaClick.trim());
---
+++ app/scripts/services/tracking-service.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//ok so here we need to overload sendAppView, sendException and sendEvent to write on local storage (JSON)
+//use local storage or couchdb service to store hits temporarilly
+//need to design the priority queue bit based on the storage capacity restriction on the local storage and introduce a table for the lost records count
+
+angular.module('lmisChromeApp')
+  .service('trackingService', function trackingService($window, $rootScope, config, utility, appConfigService) {
+    var tracker = {
+      sendAppView: function() {},
+      sendException: function() {},
+      sendEvent: function() {},
+      set: function() {}
+    };
+
+    function setUUID(config) {
+      tracker.set('userId', config.uuid);
+    }
+
+    function registerListeners() {
+      $rootScope.$on('$stateChangeSuccess', function(event, state) {
+        tracker.sendAppView(state.name);
+      });
+      $rootScope.$on('$stateNotFound', function(event, state) {
+        tracker.sendException(state.to, false);
+      });
+    }
+
+    if (utility.has($window, 'analytics')) {
+      var service = $window.analytics.getService(config.analytics.service);
+      tracker = service.getTracker(config.analytics.propertyID);
+
+      appConfigService.getCurrentAppConfig()
+        .then(setUUID)
+        .then(registerListeners);
+    }
+
+    this.tracker = tracker;
+  });
