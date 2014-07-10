05cd387d937d89becd1c25613b68088688cef875
Tom Vincent <git@tlvince.com>
2014-07-10 11:27:51 +0100

    Track by appConfig UUID, closes item:668

--- app/scripts/services/tracking-factory.js
+++ app/scripts/services/tracking-factory.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .factory('trackingFactory', function($window, $rootScope, config, utility) {
+  .factory('trackingFactory', function($window, $rootScope, config, utility, appConfigService) {
 
     var tracker;
     if (utility.has($window, 'analytics')) {
       var service = $window.analytics.getService(config.analytics.service);
       tracker = service.getTracker(config.analytics.propertyID);
+
+      appConfigService.getCurrentAppConfig()
+        .then(function(config) {
+          tracker.set('userId', config.uuid);
+        });
 
       $rootScope.$on('$stateChangeSuccess', function(event, state) {
         tracker.sendAppView(state.name);
