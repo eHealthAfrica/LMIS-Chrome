3c331a0cfb4b0e2cdf32b035ebf5c6e7daf75460
thanasis <a.anthopoulos@ucl.ac.uk>
2014-07-12 21:30:03 +0100

    Merge branch 'feature/offline-analytics' of https://github.com/thanasio/LMIS-Chrome into feature/offline-analytics
    Conflicts:
    	app/index.html
    	app/scripts/app.js
    	app/scripts/controllers/index.js
    	app/scripts/fixtures/analytics_lost_records.json
    	app/scripts/services/analytics-sync-service.js
    	app/scripts/services/app-config-service.js
    	app/scripts/services/stock-count-factory.js
    	app/scripts/services/storage-service.js
    	app/scripts/services/tracking-factory.js

--- app/scripts/directives/analytics.js
+++ app/scripts/directives/analytics.js
@@ -4,10 +4,8 @@ angular.module('lmisChromeApp')
   .directive('gaClick', function(trackingFactory) {
     return {
       restrict: 'A',
-      link: function(scope, element, attr) {
-//        var tracker = tracker;
+      link: function( scope, element, attr) {
         element.on('click', function() {
-            console.log('Click: '+ element.text().trim()+": "+ attr.gaClick.trim());
           trackingFactory.postEvent('Click', element.text().trim(), attr.gaClick.trim());
         });
       }
