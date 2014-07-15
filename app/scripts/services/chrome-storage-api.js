241336dc98ebab4629e7b9e69d59a6c1f6a559df
Tom Vincent <git@tlvince.com>
2014-07-15 15:41:12 +0100

    Revert "Farewell chrome storage"
    This reverts commit 96f4f54d1cf9787c769f2bb60faa5157891cd87d.
    
    We need it for the migration workflow.
    
    Conflicts:
    	app/scripts/services/storage-service.js

---
+++ app/scripts/services/chrome-storage-api.js
@@ -0,0 +1,86 @@
+'use strict';
+
+angular.module('lmisChromeApp').factory('chromeStorageApi', function ($window, $q, utility) {
+  var chromeStorage;
+
+  // Check whether chrome.storage available or not
+  if (utility.has($window, 'chrome.storage.local')) {
+    chromeStorage = $window.chrome.storage.local;
+  } else {
+    chromeStorage = null;
+  }
+
+  return {
+    set: function (obj) {
+      var deferred = $q.defer();
+      if (chromeStorage) {
+        chromeStorage.set(obj, function () {
+          if ($window.chrome.runtime.lastError !== undefined) {
+            deferred.reject($window.chrome.runtime.lastError);
+          } else {
+            deferred.resolve(true);
+          }
+        });
+      } else {
+        deferred.reject('chrome.storage api is not available');
+      }
+
+      return deferred.promise;
+    },
+    /*
+     * Get method should work for both cases returning particular item or entire collection.
+     * @param {Object} options - if undefined, default value is return data of a particular item
+     * or entire collection (if set to {collection:true})
+     */
+    get: function (item, options) {
+      var deferred = $q.defer();
+      if (chromeStorage) {
+        chromeStorage.get(item, function (data) {
+          if ($window.chrome.runtime.lastError !== undefined) {
+            deferred.reject($window.chrome.runtime.lastError);
+          } else {
+            if (options && options.collection) {
+              deferred.resolve(data);
+            } else {
+              deferred.resolve(data[item]);
+            }
+          }
+        });
+      } else {
+        deferred.reject('chrome.storage api is not available');
+      }
+
+      return deferred.promise;
+    },
+    remove: function (items) {
+      var deferred = $q.defer();
+      if (chromeStorage) {
+        chromeStorage.remove(items, function () {
+          if ($window.chrome.runtime.lastError !== undefined) {
+            deferred.reject($window.chrome.runtime.lastError);
+          }else{
+            deferred.resolve(true);
+          }
+        });
+      }else {
+        deferred.reject('chrome.storage api is not available');
+      }
+      return deferred.promise;
+    },
+    clear: function () {
+      var deferred = $q.defer();
+      if (chromeStorage) {
+        chromeStorage.clear(function () {
+          if ($window.chrome.runtime.lastError !== undefined) {
+            deferred.reject($window.chrome.runtime.lastError);
+          }else{
+            deferred.resolve(true);
+          }
+        });
+      } else {
+        deferred.reject('chrome.storage api is not available');
+      }
+      return deferred.promise;
+    }
+  };
+});
---
+++ test/spec/services/chrome-storage-api.js
@@ -0,0 +1,45 @@
+'use strict';
+
+describe('chromeStorageApi', function () {
+    var chromeStorageApi, $window;
+
+    beforeEach(module('lmisChromeApp', 'chromeStorageMock', 'i18nMocks'));
+
+    beforeEach(inject(function(_chromeStorageApi_, _$window_) {
+        chromeStorageApi = _chromeStorageApi_;
+        $window = _$window_;
+    }));
+
+    beforeEach(function(){
+        chromeStorageApi.set({a:'b', c:'d'});
+    });
+
+
+    it('should be able to set data to the storage', function() {
+        chromeStorageApi.set({e:'f'});
+        expect($window._storageMock).toEqual({a:'b', c:'d', e:'f'});
+    });
+
+    it('should be able to get an item from the storage', function() {
+        chromeStorageApi.get('a');
+        expect($window.chrome.storage.local.get('a')).toEqual('b');
+    });
+
+    it('should be able to get entire collection from the storage', function() {
+        var options = {collection:true};
+        chromeStorageApi.get(null, options);
+        expect($window.chrome.storage.local.get(null, options)).toEqual({a:'b', c:'d'});
+    });
+
+    it('should be able to remove an item from the storage', function() {
+        chromeStorageApi.remove('c');
+        expect($window._storageMock).toEqual({a:'b'});
+    });
+
+    it('should be able to remove all item from the storage', function() {
+        expect($window._storageMock).toEqual({a:'b', c:'d'});
+        chromeStorageApi.clear();
+        expect($window._storageMock).toEqual({});
+    });
+
+});
