293375a831178f41e6cee0e95876516223a18867
thanasis <a.anthopoulos@ucl.ac.uk>
2014-07-15 22:38:53 +0100

    Merge origin/feature/offline-analytics into feature/offline-analytics
    Conflicts:
    	app/scripts/directives/analytics.js
    	app/scripts/services/analytics-sync-service.js
    	app/scripts/services/tracking-factory.js

--- app/scripts/services/analytics-sync-service.js
+++ app/scripts/services/analytics-sync-service.js
@@ -1,97 +1,58 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .service('analyticsSyncService', function($q, storageService, trackingFactory) {
-    var tracker = trackingFactory.tracker;
-    
-    //syncs clicks events
-    this.syncClicks = function() {
-      var uuids = [];
-      storageService.all(storageService.CLICKS)
-        .then(function(clicksData) {
-          clicksData.forEach(function(click) {
-            tracker.sendEvent('Offline Clicks', click.action, click.label);
-            uuids.push(click.uuid);
-          });
-        })
-        .then(function() {
-          if (uuids.length>0) {
-            storageService.removeRecords(storageService.CLICKS, uuids);
-          }
-        })
-        .finally(function() {
-          console.log('pending clicks list cleared ');
+        .service('analyticsSyncService', function($q, storageService, trackingFactory) {
+            var tracker = trackingFactory.tracker;
+
+            //syncs analytics table
+            this.syncAnalyticsTable = function(table, code) {
+                var uuids = [];
+                storageService.all(table)
+                        .then(function(tableData) {
+                            tableData.forEach(function(data) {
+                                if (code === 0)
+                                    tracker.sendEvent('Offline Clicks', data.action, data.label);//clicks
+                                if (code === 1)
+                                    tracker.sendAppView(data.page);//pages
+                                if (code === 2)
+                                    tracker.sendException(data.opt_description, data.opt_fatal);//exceptions
+
+                                uuids.push(data.uuid);
+                            });
+                        })
+                        .then(function() {
+                            if (uuids.length > 0) {
+                                storageService.removeRecords(table, uuids);
+                            }
+                        })
+                        .finally(function() {
+                            console.log('pending table list cleared');
+                        });
+
+            };
+
+            //syncs analytics lost records
+            this.syncLostRecords = function(table, string) {
+                storageService.all(table)
+                        .then(function(data) {
+
+                            if (data.length > 0) {
+                                var obj = data[0];
+                                var records = obj.records;
+                                tracker.sendEvent("lost data", string, "", records);
+                                
+                                return obj;
+                            }
+
+                        })
+                        .then(function(obj) {
+                            if (obj)
+                                storageService.removeRecord(table, obj.uuid);
+                        })
+                        .finally(function() {
+                            console.log('pending lost analytics list cleared');
+                        });
+
+            };
+
         });
-    };
-
-    //syncs analytics exceptions
-    this.syncExceptions = function() {
-      var uuids = [];
-      storageService.all(storageService.EXCEPTIONS)
-        .then(function(exceptionData) {
-          exceptionData.forEach(function(exception) {
-            console.log('except uuid: ' + exception.uuid);
-            //best to get a success flag here and delete if event successfuly sent
-            uuids.push(exception.uuid);
-          });
-        })
-        .then(function() {
-            if (uuids.length>0){
-          storageService.removeRecords(storageService.EXCEPTIONS, uuids);
-            }
-        })
-        .finally(function() {
-          console.log('pending excepts list cleared');
-        });
-    };
-
-    //syncs analytics page views
-    this.syncPageViews = function() {
-      var uuids = [];
-      storageService.all(storageService.PAGEVIEWS)
-        .then(function(pageViewData) {
-          pageViewData.forEach(function(pageView) {
-            tracker.sendAppView(pageView.page);
-            uuids.push(pageView.uuid);
-          });
-        })
-        .then(function() {
-          if (uuids.length>0) {
-            storageService.removeRecords(storageService.PAGEVIEWS, uuids);
-          }
-        })
-        .finally(function() {
-          console.log('pending pages list cleared');
-        });
-
-    };
-    
-    //syncs analytics lost records
-    this.syncLostRecords = function() {
-      storageService.all(storageService.ANALYTICS_LOST_RECORDS)
-        .then(function(data) {
-          
-          if(data.length>0){
-            var obj = data[0];
-            var clicks = obj.clicks;
-            var exceptions = obj.exceptions;
-            var pages = obj.pages;
-
-            tracker.sendEvent("lost data","clicks", "",  clicks);
-            tracker.sendEvent("lost data","exceptions", "",  exceptions);
-            tracker.sendEvent("lost data","pages", "",  pages);
-            return obj;
-          }
-            
-        })
-        .then(function(obj) {
-          if (obj)
-            storageService.removeRecord(storageService.ANALYTICS_LOST_RECORDS, obj.uuid);
-        })
-        .finally(function() {
-          console.log('pending lost analytics list cleared');
-        });
-
-    };
-    
-  });
