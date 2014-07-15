e6b304b8c50006a7c0bf5e2de2f9d7ad78574737
Tom Vincent <git@tlvince.com>
2014-07-15 22:03:21 +0100

    Finish migrator, closes item:746

--- app/scripts/services/pouch-migration-service.js
+++ app/scripts/services/pouch-migration-service.js
@@ -2,10 +2,37 @@
 
 // TODO: remove. See item:751
 angular.module('lmisChromeApp')
-  .service('pouchMigrationService', function($q, $window, growl, i18n, chromeStorageApi, storageService, utility) {
-    function isMigrationRequired() {
+  .service('pouchMigrationService', function($q, $log, $window, growl, i18n, chromeStorageApi, storageService, utility) {
+    var versionNeedingMigration = '0.10.4';
+    var migrationFlag = '_hasMigrated';
+
+    function setMigratedFlag() {
+      var flag = {};
+      flag[migrationFlag] = true;
+      return chromeStorageApi.set(flag);
+    }
+
+    function getMigratedFlag() {
+      return chromeStorageApi.get(migrationFlag, {
+        collection: true
+      });
+    }
+
+    function isMigratable() {
       var manifest = $window.chrome.runtime.getManifest();
-      return manifest.version === '0.10.4';
+      return manifest.version === versionNeedingMigration;
+    }
+
+    function isMigrationRequired(doc) {
+      if (!utility.has(doc, migrationFlag)) {
+        return isMigratable();
+      }
+    }
+
+    function migrationRequired() {
+      var msg = i18n('migrationRequired');
+      $log.info(msg);
+      growl.info(msg);
     }
 
     function getChromeStorage() {
@@ -27,16 +54,17 @@ angular.module('lmisChromeApp')
       return $q.all(promises);
     }
 
-    function migrate() {
-      if (!isMigrationRequired()) { return; }
-      var msg = i18n('migrationRequired');
-      growl.info(msg);
-      return getChromeStorage()
-        .then(filterCollections)
-        .then(chromeToPouch);
-    }
-
     this.migrate = function() {
-      return $q.when(migrate());
+      return getMigratedFlag()
+        .then(isMigrationRequired)
+        .then(function(required) {
+          if (required) {
+            migrationRequired();
+            return getChromeStorage()
+              .then(filterCollections)
+              .then(chromeToPouch)
+              .then(setMigratedFlag);
+          }
+        });
     };
   });
