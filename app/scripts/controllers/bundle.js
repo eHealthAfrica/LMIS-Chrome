c9331eba3bab74a59734c30293cc622db94a54ef
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-08-07 18:12:05 +0100

    adds controllers for add incoming and incoming bundle history.. refs item:819

--- app/scripts/controllers/bundle.js
+++ app/scripts/controllers/bundle.js
@@ -1,149 +1,137 @@
 'use strict';
 
-angular.module('lmisChromeApp').config(function ($stateProvider) {
-  $stateProvider.state('incomingLog', {
-    url: '/incoming-log',
-    templateUrl: '/views/bundles/incoming-log.html',
-    controller: 'LogIncomingCtrl',
-    resolve: {
-      bundleNumbers: function (bundleFactory) {
-        return bundleFactory.getBundleNumbers();
+angular.module('lmisChromeApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('logIncomingHome', {
+        parent: 'root.index',
+        url: '/log-incoming-home',
+        templateUrl: '/views/bundles/index.html',
+        controller: 'LogIncomingHomeCtrl',
+        resolve: {
+          incomingBundles: function(bundleService){
+            return bundleService.getAll();
+          }
+        },
+        data: {
+          label: 'Incoming Home'
+        }
+      })
+      .state('logIncoming', {
+      parent: 'root.index',
+      url: '/log-incoming?preview&uuid',
+      templateUrl: '/views/bundles/log-incoming.html',
+      controller: 'LogIncomingCtrl',
+      resolve: {
+        appConfig: function(appConfigService){
+          return appConfigService.getCurrentAppConfig();
+        }
       },
-      currentFacilityStorageUnits: function (storageUnitFactory) {
-        return storageUnitFactory.getStorageUnitsByCurrentFacility();
+      data: {
+        label: 'Log Incoming'
       }
-    },
-    data: {
-      label: 'Log Incoming'
-    }
-  });
-})
-
-/**
- * LogIncomingCtrl for logging incoming bundle and updating inventory batch list view, bundle status, generates and stores
- * Bundle Receipt.
- */
-    .controller('LogIncomingCtrl', function ($scope, $filter, $state, bundleNumbers, storageUnitFactory, i18n, currentFacilityStorageUnits, bundleFactory, userFactory, growl) {
-
-      $scope.LOG_STEPS = {ENTER_BUNDLE_NO: 1, BUNDLE_NOT_FOUND: 2, VERIFY: 3, CONFIRM: 4};
-      $scope.bundleNumbersAutoCompleteList = bundleNumbers;
-      $scope.clicked = false;
-      $scope.bundle = {};
-      $scope.currentStep = 1;
-      $scope.receivingFacilityStorageUnits = currentFacilityStorageUnits;
-      $scope.logIncomingForm = {
-        dateReceipt: $filter('date')(new Date(), 'yyyy-MM-dd')
-      };
-      $scope.logIncomingForm.verify = [];
-      $scope.logIncomingForm.storage_units = [];
-
-      /**
-       * this is used to return storage unit object at preview page based on the uuid.
-       * */
-      $scope.getStorageUnit = function (storageUnitUUID) {
-        for (var index in $scope.receivingFacilityStorageUnits) {
-          var storageUnit = $scope.receivingFacilityStorageUnits[index];
-          if (storageUnit.uuid === storageUnitUUID) return storageUnit;
-        }
-        return {};
-      };
-
-      userFactory.getLoggedInUser().then(function (data) {
-        $scope.loggedInUser = data;
-      });
-
-      $scope.getProductType = function (bundleLine) {
-        return bundleLine.batch.product;
-      };
-
-      $scope.getQuantityDetail = function (quantity, uom) {
-        return $filter('number')(quantity, 0) + ' ' + uom.symbol;
-      };
-
-      /**
-       * function that shows form used to log incoming bundle if it already exists in the system.
-       */
-      $scope.showBundle = function () {
-
-        if ($scope.show === true && $scope.showPreview === true) {
-          $scope.showPreview = false;
-          $scope.currentStep = $scope.LOG_STEPS.VERIFY;
-          return;
-        }
-
-        $scope.clicked = true;
-        bundleFactory.getBundle($scope.showBundleNo).then(function (data) {
-          $scope.bundle = data;
-          $scope.show = true;
-          $scope.currentStep = $scope.LOG_STEPS.VERIFY;
-          $scope.showAddManually = false;
-          return;
-        }, function (error) {
-          $scope.currentStep = $scope.LOG_STEPS.BUNDLE_NOT_FOUND;
-          $scope.showAddManually = true;
-        });
-      };
-
-      /**
-       * Function used to hide form used to log incoming bundle form.
-       */
-      $scope.hideBundle = function () {
-        $scope.currentStep = $scope.LOG_STEPS.ENTER_BUNDLE_NO;
-        $scope.clicked = false;
-        $scope.show = false;
-        $scope.showAddManually = false;
-      };
-
-      /**
-       * shows preview page after entering incoming bundle details. validations and display of error message takes place
-       * here.
-       */
-      $scope.previewLogBundleForm = function () {
-        //TODO: add validations when they have been defined
-        $scope.currentStep = $scope.LOG_STEPS.CONFIRM;
-        $scope.showPreview = true;
-      };
-
-
-      /**
-       * Function called when confirm button is clicked and it saves the bundle info, to generate bundle receipt.
-       */
-      $scope.confirm = function () {
-        var bundleReceiptLines = [];
-        for (var index in $scope.bundle.bundle_lines) {
-          var bundleLine = $scope.bundle.bundle_lines[index];
-          var bundleReceiptLine = {
-            "bundle_line_uuid": bundleLine.uuid,
-            "verify": $scope.logIncomingForm.verify[bundleLine.uuid],
-            "storage_unit": $scope.logIncomingForm.storage_units[bundleLine.uuid],
-            "batch": bundleLine.batch.batch_no,
-            "program": bundleLine.program.uuid,
-            "quantity_uom": bundleLine.quantity_uom.uuid,
-            "quantity": bundleLine.quantity
-          }
-          bundleReceiptLines.push(bundleReceiptLine);
-        }
-
-        var bundleReceipt = {
-          "bundle": $scope.bundle.uuid,
-          "user": $scope.loggedInUser.id,
-          "date_receipt":  $scope.logIncomingForm.dateReceipt,
-          "bundle_receipt_lines": bundleReceiptLines,
-          "receiving_facility": $scope.bundle.receiving_facility.uuid,
-          "sending_facility": $scope.bundle.parent.uuid
-        };
-
-        bundleFactory.saveBundleReceipt(bundleReceipt).then(function (data) {
-          if (data.length !== 0) {
-            $state.go('home.index.dashboard.chart', {
-              logIncomingMsg: i18n('logIncomingSuccessMessage')
-            });
-          }
-        }, function (error) {
-          growl.error(error);
-          console.log(error);
-        });
-
-      };
     });
+  })
+  .controller('LogIncomingHomeCtrl', function($scope, incomingBundles, $state, utility, productProfileFactory){
+    $scope.incomingBundles = utility.castArrayToObject(incomingBundles, '_id');
+    $scope.previewBundle = {};
+    $scope.preview = false;
+
+    $scope.showBundle = function(bundle) {
+      for (var i in bundle.bundleLines) {
+        var ppUuid = bundle.bundleLines[i].productProfile;
+        bundle.bundleLines[i].productProfile = productProfileFactory.get(ppUuid);
+      }
+      $scope.previewBundle = bundle;
+      $scope.preview = true;
+    };
+
+    $scope.hidePreview = function(){
+      $scope.preview = false;
+    };
+
+  })
+  .controller('LogIncomingCtrl', function($scope, appConfig, productProfileFactory, bundleService, growl, $state, alertFactory, $stateParams) {
+
+    $scope.productProfiles = productProfileFactory.getAll();
+    $scope.batches = [];
+    var id = 0;
+    $scope.previewBundle = {};
+    $scope.previewForm = false;
+    //TODO: pull real facility list
+    $scope.facilities = [
+      appConfig.facility
+    ];
+    $scope.bundle = {
+      receivedOn: new Date().toJSON(),
+      receivingFacility: appConfig.facility,
+      bundleLines: []
+    };
+
+    $scope.addNewLine = function() {
+      $scope.bundle.bundleLines.push({
+        id: id++,
+        batchNo: '',
+        productProfile: ''
+      });
+    };
+
+    $scope.removeLine = function(bundleLine) {
+      $scope.bundle.bundleLines = $scope.bundle.bundleLines.filter(function(line) {
+        return line.id !== bundleLine.id;
+      });
+    };
+
+    $scope.isSelectedFacility =  function(fac){
+      var sendingFacObj = $scope.bundle.sendingFacility;
+      if(angular.isDefined(sendingFacObj) && angular.isDefined(fac)){
+        if(angular.isString(sendingFacObj) && sendingFacObj.length > 0){
+          sendingFacObj = JSON.parse(sendingFacObj);
+        }
+        return sendingFacObj.uuid === fac.uuid;
+      }
+      return false;
+    };
+
+    var updateBundleLines = function(bundle) {
+      for (var i in bundle.bundleLines) {
+        var ppUuid = bundle.bundleLines[i].productProfile;
+        bundle.bundleLines[i].productProfile = productProfileFactory.get(ppUuid);
+      }
+      return bundle;
+    };
+
+    $scope.preview = function(){
+      //TODO: Validate bundle obj and show preview if valid.
+      //TODO: create new facility obj for preview from uuid, hence no need to track currently selected facility.
+      $scope.previewForm = true;
+      $scope.previewBundle = angular.copy($scope.bundle);
+      $scope.previewBundle.sendingFacility = JSON.parse($scope.previewBundle.sendingFacility);
+      updateBundleLines($scope.previewBundle);
+    };
+
+    $scope.disableSave = function(){
+      return $scope.bundle.bundleLines.length === 0 || $scope.bundle.sendingFacility.length === 0;
+    };
+
+    $scope.showForm = function(){
+      $scope.previewForm = false;
+    };
+
+    $scope.finalSave = function(){
+      var bundle = angular.copy($scope.bundle);
+      bundle.sendingFacility = JSON.parse(bundle.sendingFacility);
+      bundleService.save(bundle)
+        .then(function() {
+          alertFactory.success('Incoming bundle logged successfully.');
+          $state.go('home.index.home.mainActivity');
+          //TODO: sync
+        })
+        .catch(function(error) {
+          console.error(error);
+          growl.error('Save incoming bundle failed, contact support.');
+        });
+    };
+
+  });
 
