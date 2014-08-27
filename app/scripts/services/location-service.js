88a811daafd47973480b9e924e69cb5ea45358a5
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-08-27 10:10:12 +0100

    adds getLga, getWards to locationService. refs item:835

---
+++ app/scripts/services/location-service.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('lmisChromeApp')
+  .service('locationService', function(storageService) {
+
+    this.getLgas = function(stateId) {
+      //TODO: refactor later run query in view.
+      return storageService.all(storageService.LOCATIONS)
+        .then(function(locs) {
+          return locs
+            .filter(function(l) {
+              return l.doc_type === 'lga' && l.parent === stateId;
+            })
+            .sort(sort);
+        });
+    };
+
+    this.getWards = function(lgaId) {
+      return storageService.all(storageService.LOCATIONS)
+        .then(function(locs) {
+          return locs
+            .filter(function(l) {
+              return l.doc_type === 'ward' && l.parent === lgaId;
+            })
+            .sort(sort);
+        });
+    };
+
+    this.saveBatch = function(locations) {
+      return storageService.setDatabase(storageService.LOCATIONS, locations);
+    };
+
+    var sort = function(a, b) {
+      var aName = a.name.toLowerCase();
+      var bName = b.name.toLowerCase();
+      if (aName < bName) {
+        return -1;
+      } else if (aName > bName) {
+        return  1;
+      } else {
+        return 0;
+      }
+    };
+
+  });
