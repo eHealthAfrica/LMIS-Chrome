e7c7d4c988744741aefb0227bba706ae194aa02c
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-08-27 10:12:58 +0100

    code reformatting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,7 @@ angular.module('lmisChromeApp', [
     'ngAnimate',
     'db'
   ])
-  .run(function(storageService, $rootScope, $state, $window, appConfigService, backgroundSyncService, fixtureLoaderService, growl, utility, pouchMigrationService, $log, i18n, analyticsSyncService) {
+  .run(function(storageService, facilityFactory, locationService, $rootScope, $state, $window, appConfigService, backgroundSyncService, fixtureLoaderService, growl, utility, pouchMigrationService, $log, i18n, analyticsSyncService) {
 
     function navigateToHome() {
       $state.go('home.index.home.mainActivity');
@@ -23,7 +23,7 @@ angular.module('lmisChromeApp', [
       analyticsSyncService.syncOfflineAnalytics()
         .finally(function() {
           console.log('offline reports send to ga server.');
-      });
+        });
     }
 
     $window.showSplashScreen = function() {
@@ -59,37 +59,37 @@ angular.module('lmisChromeApp', [
       //TODO: figure out a better way of knowing if the app has been configured or not.
       $state.go('loadingFixture');
       storageService.all(storageService.APP_CONFIG)
-      .then(function(res) {
-        if (res.length > 0) {
-          fixtureLoaderService.loadLocalDatabasesIntoMemory(fixtureLoaderService.REMOTE_FIXTURES)
-            .then(function() {
-              $rootScope.$emit('MEMORY_STORAGE_LOADED');
-              navigateToHome();
-            })
-            .catch(function(reason) {
-              console.error(reason);
-              growl.error('loading storage into memory failed, contact support.', {ttl: -1});
-            });
-        } else {
-          var loadRemoteFixture = function() {
-            return fixtureLoaderService.setupLocalAndMemoryStore(fixtureLoaderService.REMOTE_FIXTURES)
+        .then(function(res) {
+          if (res.length > 0) {
+            fixtureLoaderService.loadLocalDatabasesIntoMemory(fixtureLoaderService.REMOTE_FIXTURES)
+              .then(function() {
+                $rootScope.$emit('MEMORY_STORAGE_LOADED');
+                navigateToHome();
+              })
               .catch(function(reason) {
                 console.error(reason);
-                growl.error('Local databases and memory storage setup failed, contact support.', {ttl: -1});
+                growl.error('loading storage into memory failed, contact support.', {ttl: -1});
+              });
+          } else {
+            var loadRemoteFixture = function() {
+              return fixtureLoaderService.setupLocalAndMemoryStore(fixtureLoaderService.REMOTE_FIXTURES)
+                .catch(function(reason) {
+                  console.error(reason);
+                  growl.error('Local databases and memory storage setup failed, contact support.', {ttl: -1});
+                });
+            }
+            storageService.clear()
+              .then(loadRemoteFixture)
+              .catch(function(error) {
+                growl.error('Fresh install setup failed, please contact support.', {ttl: -1});
+                console.error(error);
               });
           }
-          storageService.clear()
-            .then(loadRemoteFixture)
-            .catch(function(error){
-              growl.error('Fresh install setup failed, please contact support.', {ttl: -1});
-              console.error(error);
-            });
-        }
-      })
-      .catch(function(error) {
-        growl.error('loading of App. Config. failed, please contact support.', {ttl: -1});
-        console.error(error);
-      });
+        })
+        .catch(function(error) {
+          growl.error('loading of App. Config. failed, please contact support.', {ttl: -1});
+          console.error(error);
+        });
     }
 
     function migrationErrorHandler(err) {
