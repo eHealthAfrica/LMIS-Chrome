e9d2935d64d46d59b586597f90c73b08c78102e5
Tom Vincent <git@tlvince.com>
2014-10-15 16:49:36 +0100

    Build VVM options list via a map

--- app/scripts/controllers/bundle.js
+++ app/scripts/controllers/bundle.js
@@ -173,7 +173,7 @@ angular.module('lmisChromeApp')
       'Stage 3'
     ];
   })
-  .controller('LogBundleCtrl', function($scope, batchStore, utility, batchService, appConfig, i18n, productProfileFactory, bundleService, growl, $state, alertFactory, syncService, $stateParams, $filter, locationService, facilityFactory,appConfigService,productCategoryFactory) {
+  .controller('LogBundleCtrl', function($scope, batchStore, utility, batchService, appConfig, i18n, productProfileFactory, bundleService, growl, $state, alertFactory, syncService, $stateParams, $filter, locationService, facilityFactory,appConfigService,productCategoryFactory, VVM_OPTIONS) {
 
     var logIncoming = bundleService.INCOMING;
     var logOutgoing = bundleService.OUTGOING;
@@ -187,6 +187,7 @@ angular.module('lmisChromeApp')
     $scope.err = {};
     $scope.batchNos = Object.keys(batchStore);
 
+    $scope.vvmOptions = VVM_OPTIONS;
     $scope.selectVVMOption = function(bundleLine,option){
       $scope.toggleIsopen = false;
       bundleLine.VVMStatus = option;
