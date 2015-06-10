2767b079602139a850b80d8c790291d2a37d2c94
jofomah <jideobi.ofomah@ehealthnigeria.org>
2015-06-10 15:20:29 +0100

    load data into memory store on restart.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,48 +1,78 @@
 'use strict';
 
 angular.module('lmisChromeApp', [
-    'ui.bootstrap',
-    'ui.router',
-    'tv.breadcrumbs',
-    'pouchdb',
-    'config',
-    'nvd3ChartDirectives',
-    'angular-growl',
-    'ngAnimate',
-    'db',
-    'LocalForageModule'
-  ])
-  .run(function(storageService, facilityFactory, locationService, $rootScope, $state, $window, appConfigService, backgroundSyncService, fixtureLoaderService, growl, utility, pouchMigrationService, $log, i18n, analyticsSyncService) {
+	'ui.bootstrap',
+	'ui.router',
+	'tv.breadcrumbs',
+	'pouchdb',
+	'config',
+	'nvd3ChartDirectives',
+	'angular-growl',
+	'ngAnimate',
+	'db',
+	'LocalForageModule'
+])
+		.run(function (storageService, facilityFactory, locationService, $localForage, $rootScope, $state, $window, appConfigService, backgroundSyncService, fixtureLoaderService, growl, utility, pouchMigrationService, $log, i18n, analyticsSyncService) {
 
-    // TODO: see item:680
-    if (!utility.has($window, 'PouchDB')) {
-      // Short-circuit as PouchDB has not been sourced. Likely running in test
-      // environment.
-      return;
-    }
-    appConfigService.getCurrentAppConfig()
-      .then(function(cfg) {
-        if (angular.isObject(cfg) && !angular.isArray(cfg)) {
-          $state.go('home.index.home.mainActivity');
-        } else {
-          $state.go('appConfigWelcome');
-        }
-      })
-      .catch(function(reason) {
-        growl.error('loading of App. Config. failed, please contact support.');
-        console.error(reason);
-      });
+			// TODO: see item:680
+			if (!utility.has($window, 'PouchDB')) {
+				// Short-circuit as PouchDB has not been sourced. Likely running in test
+				// environment.
+				return;
+			}
 
-  })
-  .config(function($compileProvider) {
-    // to bypass Chrome app CSP for images.
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(chrome-extension):/);
-  })
-  .config(function(growlProvider) {
-    growlProvider.globalTimeToLive({
-      success: 5000,
-      error: 5000,
-      warning: 5000,
-      info: 5000
-    });
-  });
+			function navigateToHome() {
+				$state.go('home.index.home.mainActivity');
+				backgroundSyncService.startBackgroundSync()
+						.finally(function () {
+							console.log('updateAppConfigAndStartBackgroundSync triggered on start up has been completed!');
+						});
+
+				analyticsSyncService.syncOfflineAnalytics()
+						.finally(function () {
+							console.log('offline reports send to ga server.');
+						});
+			}
+
+			function loadApp() {
+				storageService.get(storageService.APP_CONFIG)
+						.then(function (data) {
+							if (angular.isArray(data) && data.length > 0) {
+								var appConfig = data[0];
+								if (angular.isObject(appConfig)) {
+									fixtureLoaderService.loadLocalDatabasesIntoMemory(fixtureLoaderService.REMOTE_FIXTURES)
+											.then(function () {
+												$rootScope.$emit('MEMORY_STORAGE_LOADED');
+												navigateToHome();
+											})
+											.catch(function (reason) {
+												console.error(reason);
+												growl.error('loading storage into memory failed, contact support.', {ttl: -1});
+											});
+								} else {
+									growl.error('App configuration is not valid, contact support.');
+								}
+							} else {
+								$state.go('appConfigWelcome');
+							}
+						})
+						.catch(function () {
+							growl.error('Loading of App. Config. failed, please contact support.', {ttl: -1});
+						});
+			}
+
+			loadApp();
+
+		})
+		.config(function ($compileProvider) {
+			// to bypass Chrome app CSP for images.
+			$compileProvider.imgSrcSanitizationWhitelist(/^\s*(chrome-extension):/);
+		})
+		.config(function (growlProvider) {
+			growlProvider.globalTimeToLive({
+				success: 5000,
+				error: 5000,
+				warning: 5000,
+				info: 5000
+			});
+		});
